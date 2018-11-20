BUG: Fixed radial average of 2D autocorrelation function.

PyCo/Topography/Autocorrelation.py
@@ -144,7 +144,7 @@ def autocorrelation_2D(surface_xy, nbins=100,  # pylint: disable=invalid-name
 
     # Compute FFT and normalize
     if periodic:
-        surface_qk = np.fft.fft2(surface_xy[:, :])
+        surface_qk = np.fft.fft2(surface_xy.array())
         C_qk = abs(surface_qk) ** 2  # pylint: disable=invalid-name
         A_xy = np.fft.ifft2(C_qk).real / (nx * ny)
 
@@ -159,7 +159,7 @@ def autocorrelation_2D(surface_xy, nbins=100,  # pylint: disable=invalid-name
         r_edges, n, r_val, A_val = radial_average(  # pylint: disable=invalid-name
             A_xy, (sx + sy) / 4, nbins, size=(sx, sy))
     else:
-        p = surface_xy[...]
+        p = surface_xy.array()
 
         # Compute height-height autocorrelation function
         surface_qk = np.fft.fft2(p, s=(2 * nx - 1, 2 * ny - 1))
@@ -180,7 +180,7 @@ def autocorrelation_2D(surface_xy, nbins=100,  # pylint: disable=invalid-name
 
         # Radial average
         r_edges, n, r_val, A_val = radial_average(  # pylint: disable=invalid-name
-            A_xy, (sx + sy) / 2, nbins, size=(sx * (2 * nx - 1) / nx, sy * (2 * ny - 1) / ny))
+            A_xy, (sx + sy) / 2, nbins, size=(sx, sy), full=False)
 
     if return_map:
         return r_val[n > 0], A_val[n > 0], A_xy

PyCo/Topography/common.py
@@ -69,7 +69,7 @@ def compute_derivative(profile, size=None, dim=None, n=1):
             for d in dims]
 
 
-def radial_average(C_xy, rmax, nbins, size=None):
+def radial_average(C_xy, rmax, nbins, size=None, full=True):
     """
     Compute radial average of quantities reported on a 2D grid.
 
@@ -82,8 +82,13 @@ def radial_average(C_xy, rmax, nbins, size=None):
     nbins : int
         Number of bins for averaging.
     size : (float, float), optional
-        Physical size of the 2D grid. (Default: Size is equal to number of grid
-        points.)
+        Physical size of the 2D grid. (Default: Size is equal to number of
+        grid points.)
+    full : bool
+        Number of quadrants contained in data. (Default: True)
+        True: Full radial average from 0 to 2*pi.
+        False: Only the one quarter of the full circle is present. Radial
+        average from 0 to pi/2.
 
     Returns
     -------
@@ -100,9 +105,12 @@ def radial_average(C_xy, rmax, nbins, size=None):
     nx, ny = C_xy.shape
     sx = sy = 1.
     x = np.arange(nx)
-    x = np.where(x > nx//2, nx-x, x)/nx
     y = np.arange(ny)
-    y = np.where(y > ny//2, ny-y, y)/ny
+    if full:
+        x = np.where(x > nx//2, nx-x, x)
+        y = np.where(y > ny//2, ny-y, y)
+    x = x/nx
+    y = y/ny
 
     rmin = 0.0
 