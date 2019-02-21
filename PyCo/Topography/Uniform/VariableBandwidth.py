85865c96702c410031701bcd0deeb58f9c35f94c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-02-21 21:48:32 +0100

    ENH: Speed-up variable bandwidth analysis for topgraphies. Added analysis function and tests to uniform line scans.

--- PyCo/Topography/Uniform/VariableBandwidth.py
+++ PyCo/Topography/Uniform/VariableBandwidth.py
@@ -67,32 +67,58 @@ def checkerboard_detrend(topography, subdivisions):
     size = topography.size
     nb_dim = topography.dim
 
+    shape = arr.shape
+
     # compute unique consecutive index for each subdivided region
-    region_coord = [np.arange(arr.shape[i]) * subdivisions[i] // arr.shape[i]
-                    for i in range(nb_dim)]
+    region_coord = [np.arange(shape[i]) * subdivisions[i] // shape[i] for i in range(nb_dim)]
     if nb_dim > 1:
         region_coord = np.meshgrid(*region_coord, indexing='ij')
     region_index = region_coord[0]
     for i in range(1, nb_dim):
         region_index = subdivisions[i] * region_index + region_coord[i]
 
-    x_grids = (np.arange(arr.shape[i]) for i in range(nb_dim))
-    if nb_dim > 1:
-        x_grids = np.meshgrid(*x_grids, indexing='ij')
-    full_columns = [x.reshape((-1, 1)) for x in x_grids]
+    # compute x- and y-coordinate
+    if nb_dim == 1:
+        x = np.arange(shape[0])
+    elif nb_dim == 2:
+        x, y = np.meshgrid(*(np.arange(n) for n in shape), indexing='ij')
+    else:
+        raise ValueError('Cannot handle {}-dimensional topographies.'.format(nb_dim))
 
-    for i in range(np.max(region_index)+1):
-        # tilt correction of each of these regions
-        mask = region_index == i
-        flat_mask = np.ravel(mask)
-        columns = [c[flat_mask] for c in full_columns]
-        columns.append(np.ones_like(columns[-1]))
-        # linear regression model
-        location_matrix = np.array(np.hstack(columns))
-        offsets = np.ravel(arr)[flat_mask]
-        res = np.linalg.lstsq(location_matrix, offsets, rcond=None)
-        # correct tilt
-        arr[mask] -= location_matrix.dot(res[0])
+    region_index.shape = (-1, )
+    x.shape = (-1, )
+    arr.shape = (-1, )
+    sum_1 = np.bincount(region_index)
+    sum_x = np.bincount(region_index, x)
+    sum_xx = np.bincount(region_index, x*x)
+    sum_h = np.bincount(region_index, arr)
+    sum_xh = np.bincount(region_index, x*arr)
+    if nb_dim == 2:
+        y.shape = (-1,)
+        sum_y = np.bincount(region_index, y)
+        sum_yy = np.bincount(region_index, y * y)
+        sum_xy = np.bincount(region_index, x * y)
+        sum_yh = np.bincount(region_index, y*arr)
+
+    if nb_dim == 1:
+        # Calculated detrended plane. Detrended plane is given by h0 + mx*x.
+        A = np.array([[sum_1, sum_x],
+                      [sum_x, sum_xx]])
+        b = np.array([sum_h, sum_xh])
+        h0, mx = np.linalg.solve(A.T, b.T).T
+
+        arr -= h0[region_index] + mx[region_index]*x
+    else:
+        # Calculated detrended plane. Detrended plane is given by h0 + mx*x + my*y.
+        A = np.array([[sum_1, sum_x, sum_y],
+                      [sum_x, sum_xx, sum_xy],
+                      [sum_y, sum_xy, sum_yy]])
+        b = np.array([sum_h, sum_xh, sum_yh])
+        h0, mx, my = np.linalg.solve(A.T, b.T).T
+
+        arr -= h0[region_index] + mx[region_index]*x + my[region_index]*y
+
+    arr.shape = shape
 
     return arr
 
@@ -137,3 +163,6 @@ def variable_bandwidth(topography, resolution_cutoff=4):
 
 Topography.register_function('checkerboard_detrend', checkerboard_detrend)
 Topography.register_function('variable_bandwidth', variable_bandwidth)
+
+UniformLineScan.register_function('checkerboard_detrend', checkerboard_detrend)
+UniformLineScan.register_function('variable_bandwidth', variable_bandwidth)
