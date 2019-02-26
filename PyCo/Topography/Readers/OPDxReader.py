4af1eef980593c43ac6cefe83376c6787ceaed1e
Kai Haase <k.o.haase@googlemail.com>
2019-02-26 11:17:33 +0100

    WIP: Reading of data and metadata now works for 2d input. TODO: output dict -> topography object; make compatible for 1d data, maybe look into speeding up matrix fetching and clean up code.

--- PyCo/Topography/Readers/OPDxReader.py
+++ PyCo/Topography/Readers/OPDxReader.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# from PyCo.Topography.ParallelFromFile import TopographyLoader
+from PyCo.Topography.ParallelFromFile import TopographyLoader
 
 # filePath = "../../../tests/file_format_examples/example5.OPDx"
 
@@ -41,7 +41,33 @@ RAW_1D_DATA = "/1D_Data/Raw"
 ANY_2D_DATA = "/2D_Data/"
 
 
-buffer = []  # The actual contents of the file
+class TopographyLoaderOPDx(TopographyLoader):
+
+    # Reads in the positions of all the data and metadata, runs quickly.
+    def __init__(self, file_path, size=None, unit=None, info=None):
+        super().__init__(size, unit, info)
+
+        with open(file_path, "rb") as f:
+            # read in file as hexadecimal
+            # buffer = [format(byte, '02x') for byte in f.read()]
+            self.buffer = [chr(byte) for byte in f.read()]
+            # length of file
+            size = len(self.buffer)
+
+            # check if correct header
+            if size < MAGIC_SIZE or ''.join(self.buffer[:MAGIC_SIZE]) != MAGIC:
+                raise ValueError('Invalid file format for Dektak OPDx.')
+
+            self.hash_table = dict()
+
+            pos = MAGIC_SIZE
+
+            while pos < size:
+                buf, pos, self.hash_table, path = read_item(buf=self.buffer, pos=pos, hash_table=self.hash_table, path="")
+
+    # Gets the actual data and metadata from the positions. Slower, but maybe able to speed this up in the future.
+    def topography(self):
+        return find_2d_data(self.hash_table, self.buffer)  # TODO: bring in correct topo format
 
 
 class DektakItemData:
@@ -112,25 +138,6 @@ class DektakBuf:
         self.position = position
         self.length = length
 
-"""
-class TopographyLoaderNPY(TopographyLoader):
-    def __init__(self, file_name, size=None, unit=None, info=None):
-        super().__init__(size, unit, info)
-        self.index_dict = load_opdx(file_name)
-
-    def topography(self):
-        pass
-"""
-
-
-def find_quantity(hash_table, key):
-    global buffer
-    item = hash_table[key]
-    position = item.data.qun.symbol.position
-    length = item.data.qun.symbol.length
-
-    s = buffer[position:position + length]
-
 
 def find_2d_data_matrix(name, item):
     if item.typeid != DEKTAK_MATRIX:
@@ -145,7 +152,8 @@ def find_2d_data_matrix(name, item):
     return name[9:s]
 
 
-def find_2d_data(hash_table):
+def find_2d_data(hash_table, buf):
+    output = dict()
     channels = []
 
     # Get a list of all channels containing 2d data matrices
@@ -154,27 +162,103 @@ def find_2d_data(hash_table):
         if found is not None:
             channels.append(found)
 
-    s = ""
-    string = ""  # probably output
-    for i in range(len(channels)):
-        string += ANY_2D_DATA
-        string += channels[i]
+    for channel in channels:
+        meta_data = create_meta(hash_table)
+
+        string = ANY_2D_DATA
+        string += channel
 
         length = len(string)
 
+        # Get position and res of data matrix
         string += "/Matrix"
+        item = hash_table[string]
+        start = item.data.matrix.buf.p
 
+        end = start + item.data.matrix.buf.length
+        xres = item.data.matrix.xres
+        yres = item.data.matrix.yres
+
+        meta_data["xres"] = xres
+        meta_data["yres"] = yres
+
+        # TODO: multiply value by interpret(item.data.qun.symbol)
         string = string[:length]
         string += "/Dimension1Extent"
+        item = hash_table[string]
+        yreal = item.data.qun.value
+        yunit = item.data.qun.symbol
 
-        find_quantity(hash_table, string)
+        meta_data["Height value"] = yreal
+        meta_data["Height unit"] = yunit
 
         string = string[:length]
         string += "/Dimension2Extent"
+        item = hash_table[string]
+        xreal = item.data.qun.value
+        xunit = item.data.qun.symbol
 
-        find_quantity(hash_table, string)
+        meta_data["Width value"] = xreal
+        meta_data["Width unit"] = xunit
 
-        string = ""
+        string = string[:length]
+        string += "/DataScale"
+        item = hash_table[string]
+        q = item.data.qun.value
+        zunit = item.data.qun.symbol
+
+        meta_data["z scale"] = q
+        meta_data["z unit"] = zunit
+
+        # TODO: maybe speed up if possible
+        data = buf[start:end]
+        pos = 0
+        rawdata = []
+        for line in range(yres):
+            row = []
+            for point in range(xres):
+                value, pos = read_float(data, pos)
+                row.append(value)
+            rawdata.append(np.array(row))
+
+        rawdata = np.array(rawdata)
+        output[channel] = (rawdata, meta_data)
+    return output
+
+
+def create_meta(hash_table):
+    container = dict()
+    for key in hash_table.keys():
+        if not key.startswith('/MetaData/'):
+            continue
+        item = hash_table[key]
+
+        if item.typeid == DEKTAK_BOOLEAN:
+            metavalue = item.data.b
+        elif item.typeid == DEKTAK_SINT32:
+            metavalue = item.data.si
+        elif item.typeid == DEKTAK_UINT32:
+            metavalue = item.data.ui
+        elif item.typeid == DEKTAK_SINT64:
+            metavalue = item.data.sq
+        elif item.typeid == DEKTAK_UINT64:
+            metavalue = item.data.uq
+        elif item.typeid == DEKTAK_DOUBLE or item.typeid == DEKTAK_FLOAT:
+            metavalue = item.data.d
+        elif item.typeid == DEKTAK_STRING:
+            metavalue = "".join(item.data.buf)
+        elif item.typeid == DEKTAK_QUANTITY:
+            metavalue = str(item.data.qun.value) + item.data.qun.symbol
+        elif item.typeid == DEKTAK_STRING_LIST:
+            metavalue = "; ".join(item.data.strlist)
+        elif item.typeid == DEKTAK_TERMINATOR:
+            metavalue = None
+        else:
+            # Not really meta data
+            continue
+        metakey = key.replace("/", "::")
+        container[metakey] = metavalue
+    return container
 
 
 def load_opdx(file_path):
@@ -182,7 +266,6 @@ def load_opdx(file_path):
         # read in file as hexadecimal
         # buffer = [format(byte, '02x') for byte in f.read()]
         buffer = [chr(byte) for byte in f.read()]
-
         # length of file
         size = len(buffer)
 
@@ -196,23 +279,25 @@ def load_opdx(file_path):
 
         while pos < size:
             buf, pos, hash_table, path = read_item(buf=buffer, pos=pos, hash_table=hash_table, path="")
-        print("done with reading")
-        find_2d_data(hash_table)
-        # container = find_1d_data(hash, container, str)
 
-        # container = find_2d_data(hash, container, str)
+        # TODO: find_1d_data
 
-        # container = gwy_container_get_n_items(container)
+        data_2d = find_2d_data(hash_table, buffer)
 
-        # return container
+        """
+        rawdata, metadata = data_2d["Image"]
+        print(rawdata.shape)
+        for key in metadata.keys():
+            print(key + ": " + str(metadata[key]))
+        """
+        return data_2d
 
 
-def find_1d_data(hash_table):
+def find_1d_data(hash_table, buf):
     raise NotImplementedError
 
 
-def read_item(buf, pos, hash_table, path):
-    print("pos: " + str(pos))
+def read_item(buf, pos, hash_table, path, abspos=0):
     orig_path_len = len(path)
     item = DektakItem()
     itempos = 0
@@ -234,11 +319,17 @@ def read_item(buf, pos, hash_table, path):
         else:
             raise ValueError("Something went wrong.")
 
-    elif item.typeid == DEKTAK_SINT32 or item.typeid == DEKTAK_UINT32:
-        item.data.ui, pos = read_int32(buf, pos)
+    elif item.typeid == DEKTAK_SINT32:
+        item.data.si, pos = read_int32(buf, pos, signed=True)
 
-    elif item.typeid == DEKTAK_SINT64 or item.typeid == DEKTAK_UINT64:
-        item.data.uq, pos = read_int64(buf, pos)
+    elif item.typeid == DEKTAK_UINT32:
+        item.data.ui, pos = read_int32(buf, pos, signed=False)
+
+    elif item.typeid == DEKTAK_SINT64:
+        item.data.sq, pos = read_int64(buf, pos, signed=True)
+
+    elif item.typeid == DEKTAK_UINT64:
+        item.data.uq, pos = read_int64(buf, pos, signed=False)
 
     elif item.typeid == DEKTAK_FLOAT:
         item.data.d, pos = read_float(buf, pos)
@@ -246,20 +337,19 @@ def read_item(buf, pos, hash_table, path):
     elif item.typeid == DEKTAK_DOUBLE:
         item.data.d, pos = read_double(buf, pos)
 
-    elif item.typeid == DEKTAK_TIME_STAMP:  # TODO: SEE IF THIS WORKS
-        print("IT WORKS")
+    elif item.typeid == DEKTAK_TIME_STAMP:
         time, pos = read_with_check(buf, pos, TIMESTAMP_SIZE)
         item.data.timestamp.append(time)
 
     elif item.typeid == DEKTAK_STRING:
-        item.data.buf, pos = read_structured(buf, pos)
+        item.data.buf, _, pos = read_structured(buf, pos)
 
     elif item.typeid == DEKTAK_QUANTITY:
-        content, pos = read_structured(buf, pos)
+        content, _, pos = read_structured(buf, pos)
         item.data.qun, itempos = read_quantunit_content(content, itempos, False)
 
     elif item.typeid == DEKTAK_UNITS:
-        content, pos = read_structured(buf, pos)
+        content, _, pos = read_structured(buf, pos)
         item.data.qun, itempos = read_quantunit_content(content, itempos, True)
 
     elif item.typeid == DEKTAK_TERMINATOR:
@@ -269,27 +359,28 @@ def read_item(buf, pos, hash_table, path):
     # Container types. Cannot tell any difference between these two.  Raw data purpose
     # seems to be wrapping actual raw data in something container-like.
     elif item.typeid == DEKTAK_CONTAINER or item.typeid == DEKTAK_RAW_DATA or item.typeid == DEKTAK_RAW_DATA_2D:
-        content, pos = read_structured(buf, pos)  # TODO find out if maybe better place somewhere else
+        content, start, pos = read_structured(buf, pos)  # TODO find out if maybe better place somewhere else
+        abspos += start
         while itempos < len(content):
-            content, itempos, hash_table, path = read_item(buf=content, pos=itempos, hash_table=hash_table, path=path)
+            content, itempos, hash_table, path = read_item(buf=content, pos=itempos, hash_table=hash_table, path=path, abspos= abspos)
 
     # Types with string type name (i.e.untyped serialised junk we have to know how to read).
     elif item.typeid == DEKTAK_DOUBLE_ARRAY:
-        item.typename, item.data.buf, pos = read_named_struct(buf, pos)
+        item.typename, item.data.buf, _, pos = read_named_struct(buf, pos)
 
     elif item.typeid == DEKTAK_STRING_LIST:
-        item.typename, content, pos = read_named_struct(buf, pos)
+        item.typename, content, start, pos = read_named_struct(buf, pos)
         item.data.strlist = []
         while itempos < len(content):
             s, itempos = read_name(content, itempos)
             item.data.strlist.append(s)
 
     elif item.typeid == DEKTAK_TYPE_ID:
-        item.typename, item.data.buf, pos = read_named_struct(buf, pos)
+        item.typename, item.data.buf, _, pos = read_named_struct(buf, pos)
 
     elif item.typeid == DEKTAK_POS_RAW_DATA:
         if path.startswith('/2D_Data'):
-            item.typename, content, pos = read_named_struct(buf, pos)
+            item.typename, content, _, pos = read_named_struct(buf, pos)
 
             item.data.rawpos2d.unitx, item.data.rawpos2d.divisorx, itempos = \
                 read_dimension2d_content(content, itempos, item.data.rawpos2d.unitx)
@@ -297,7 +388,7 @@ def read_item(buf, pos, hash_table, path):
                 read_dimension2d_content(content, itempos, item.data.rawpos2d.unity)
 
         elif path.startswith('/1_Data'):
-            item.typename, content, pos = read_named_struct(buf, pos)
+            item.typename, content, _, pos = read_named_struct(buf, pos)
             content.position += buf.position
 
             item.data.rawpos1d.unit, itempos = read_quantunit_content(content, itempos, True)
@@ -315,12 +406,14 @@ def read_item(buf, pos, hash_table, path):
         item.data.matrix.some_int, pos = read_int32(buf, pos)
         item.data.matrix.another_name, pos = read_name(buf, pos)
         item.data.matrix.buf.length, pos = read_varlen(buf, pos)
-        item.data.matrix.buf.yres, pos = read_int32(buf, pos)
-        item.data.matrix.buf.xres, pos = read_int32(buf, pos)
+        item.data.matrix.yres, pos = read_int32(buf, pos)
+        item.data.matrix.xres, pos = read_int32(buf, pos)
 
         if item.data.matrix.buf.length < 8:  # 2 * sizeof int32
             raise ValueError
         item.data.matrix.buf.length -= 8
+        item.data.matrix.buf.p = pos + abspos
+
         # TODO Here might be some error if one appears. If not, this is exactly as planned.
         if len(buf) - pos < item.data.matrix.buf.length:
             raise ValueError
@@ -335,8 +428,8 @@ def read_item(buf, pos, hash_table, path):
 
 def read_named_struct(buf, pos):
     typename, pos = read_name(buf, pos)
-    content, pos = read_structured(buf, pos)
-    return typename, content, pos
+    content, start, pos = read_structured(buf, pos)
+    return typename, content, start, pos
 
 
 def read_quantunit_content(buf, pos, is_unit):
@@ -378,7 +471,6 @@ def read_name(buf, pos):
     name = buf[position:position+length]
     name = "".join(s for s in name)
     pos += length
-    print(name)
     return name, pos
 
 
@@ -388,8 +480,7 @@ def read_structured(buf, pos):
         raise ValueError("Some sizes went wrong.")
     start = pos
     pos += length
-
-    return buf[start:start+length], pos
+    return buf[start:start+length], start, pos
 
 
 def read_varlen(buf, pos):
@@ -407,24 +498,27 @@ def read_varlen(buf, pos):
     return length, pos
 
 
-def read_int64(buf, pos):
+def read_int64(buf, pos, signed=False):
     out, pos = read_with_check(buf=buf, pos=pos, nbytes=8)
     out = ''.join(out)
-    out = np.frombuffer(str.encode(out, "raw_unicode_escape"), "<u8")[0]  # interpret hexadecimal -> int (little-endian)
+    dt = "<i8" if signed else "<u8"
+    out = np.frombuffer(str.encode(out, "raw_unicode_escape"), dt)[0]  # interpret hexadecimal -> int (little-endian)
     return out, pos
 
 
-def read_int32(buf, pos):
+def read_int32(buf, pos, signed=False):
     out, pos = read_with_check(buf=buf, pos=pos, nbytes=4)
     out = ''.join(out)
-    out = np.frombuffer(str.encode(out, "raw_unicode_escape"), "<u4")[0]  # interpret hexadecimal -> int (little-endian)
+    dt = "<i4" if signed else "<u4"
+    out = np.frombuffer(str.encode(out, "raw_unicode_escape"), dt)[0]  # interpret hexadecimal -> int (little-endian)
     return out, pos
 
 
-def read_int16(buf, pos):
+def read_int16(buf, pos, signed=False):
     out, pos = read_with_check(buf=buf, pos=pos, nbytes=2)
     out = ''.join(out)
-    out = np.frombuffer(str.encode(out, "raw_unicode_escape"), "<u2")[0]  # interpret hexadecimal -> int (little-endian)
+    dt = "<i2" if signed else "<u2"
+    out = np.frombuffer(str.encode(out, "raw_unicode_escape"), dt)[0]  # interpret hexadecimal -> int (little-endian)
     return out, pos
 
 
