d5c2366c239abcf78d762b4751951c295e51fdb9
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-04-18 22:39:04 +0200

    BUG: Fixed 'detect_format', closes #117

--- PyCo/Topography/IO/__init__.py
+++ PyCo/Topography/IO/__init__.py
@@ -33,6 +33,19 @@ from PyCo.Topography.IO.OPDx import OPDxReader
 from .Reader import UnknownFileFormatGiven, CannotDetectFileFormat, \
     FileFormatMismatch, CorruptFile
 
+readers = {
+    "asc": AscReader,
+    "npy": NPYReader,
+    "h5": H5Reader,
+    "OPDx": OPDxReader,
+    'di': DiReader,
+    'ibw': IbwReader,
+    'mat': MatReader,
+    'opd': OpdReader,
+    'x3p': X3pReader,
+    'xyz': XyzReader
+}
+
 
 def detect_format(fn, comm=None):
     """
@@ -51,21 +64,8 @@ def detect_format(fn, comm=None):
                 reader(fn)
             return name
         except:
-            raise CannotDetectFileFormat()
-
-
-readers = {
-    "asc": AscReader,
-    "npy": NPYReader,
-    "h5": H5Reader,
-    "OPDx": OPDxReader,
-    'di': DiReader,
-    'ibw': IbwReader,
-    'mat': MatReader,
-    'opd': OpdReader,
-    'x3p': X3pReader,
-    'xyz': XyzReader
-}
+            pass
+    raise CannotDetectFileFormat()
 
 
 def read(fn, format=None, comm=None):
