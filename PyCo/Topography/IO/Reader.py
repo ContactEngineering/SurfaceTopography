7b15f07b41b428dab0af287c79382e2b50d2226b
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2019-08-02 14:04:59 +0200

    DOC: Updated doc strings for readers; unified them with doc strings for topography

--- PyCo/Topography/IO/Reader.py
+++ PyCo/Topography/IO/Reader.py
@@ -28,7 +28,6 @@ import abc
 
 
 class ReaderBase(metaclass=abc.ABCMeta):
-
     def __init__(self, nb_grid_pts=None, physical_sizes=None, periodic=False, info={}):
         self._nb_grid_pts = nb_grid_pts
         self._size = physical_sizes
@@ -39,15 +38,45 @@ class ReaderBase(metaclass=abc.ABCMeta):
     @property
     def channels(self):
         """
-        List of dictionnaries describing the available channels
+        Returns a list of dictionaries describing the available data channels.
 
-        The dictionary at least contains the fields
-        ["name", "height_scale_factor", "unit"]
+        Each dict has as least the following elements:
 
-        Returns
-        -------
-        list of dicts
+        name : str
+            Name of the channel. If no name is found in the file,
+            "Default" is used. Can be used in a UI for identifying a channel.
 
+        nb_grid_pts : tuple of ints
+            Number of grid points in each direction, either 1 or 2 elements
+            depending on the dimension of the topography.
+
+        physical_sizes : tuple of floats or None
+            If the physical size can be determined from the file, a tuple is
+            returned. The tuple has 1 or 2 elements (size_x, size_y), depending
+            on the dimension of the topography.
+
+            If the physical size can not be determined from the file, then
+            None is returned.
+
+            Note that the physical sizes obtained from the file can be
+            overwritten by passing a `physical_sizes` argument to the
+            `topography` method that returns the topography object.
+
+        unit : str or tuple (str,??)
+            The unit is either a string or tuple (str, ??).
+
+            [I don't know what the tuple means and what the
+            type of the second argument can be. I only know I shouldn't
+            take those topographies for topobank.]
+
+            This is the unit of the physical size (if given in the file)
+            and the units of the heights. Please also see "height_scale_factor"
+            below.
+
+         height_scale_factor : float
+             Factor which was used to scale the raw numbers from file (which
+             can be voltages or some other quantity) to heights with the given
+             'unit'. Use `topography.heights()` in order to get the heights.
         """
         channelinfo = {"name": "Default",
                        "nb_grid_pts": self._nb_grid_pts,
@@ -60,28 +89,44 @@ class ReaderBase(metaclass=abc.ABCMeta):
 
     @property
     def default_channel(self):
-        """
-        Index of the default_channel
-        Returns
-        -------
-
-        """
+        """Return the index of the default channel."""
         return self._default_channel
 
     @property
     def nb_grid_pts(self):
+        """Return the number of grid points of the topography."""
         return self._nb_grid_pts
 
     @property
     def physical_sizes(self):
+        """Return the physical sizes of the topography."""
         return self._size
 
     @property
+    def dim(self):
+        """Returns 1 for line scans and 2 for topography maps."""
+        raise len(self._nb_grid_pts)
+
+    @property
     def is_periodic(self):
+        """Return whether the topography is periodically repeated at the boundaries."""
         return self._periodic
 
     @property
     def info(self):
+        """
+        Return the info dictionary. The info dictionary contains auxiliary data
+        found in the topography data file but not directly used by PyCo.
+
+        The dictionary can contain any type of information. There are a few
+        standardized keys, listed in the following.
+
+        Standardized keys:
+        unit : str
+            Unit of the topography. The unit information applies to the lateral
+            units (the physical size) as well as to heights units. Examples:
+            'µm', 'nm'.
+        """
         return self._info
 
     def _process_size(self, size):
