7e032c6a996d15074ccd24f3815453107cd35dcf
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2019-11-20 10:13:51 +0100

    DOC: see #198. document periodic argument in readers.

--- PyCo/Topography/IO/Reader.py
+++ PyCo/Topography/IO/Reader.py
@@ -125,6 +125,10 @@ class ReaderBase(metaclass=abc.ABCMeta):
         info : dict
             This dictionary will be appended to the info dictionary returned
             by the reader.
+        periodic: bool
+            Wether the Topography should be interpreted as one period of a
+            periodic surface. This will affect the PSD and autocorrelation
+            calculations (windowing)
         subdomain_locations : tuple of ints
             Origin (location) of the subdomain handled by the present MPI
             process.
