5d80e0ad052f386b1aee1aea0a5db2b0e82fbc5c
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-04-30 08:37:45 +0200

    BUG: fourier_interpolation is now transposition symmetric at the nyquist frequency.

--- PyCo/Topography/Uniform/Interpolation.py
+++ PyCo/Topography/Uniform/Interpolation.py
@@ -65,29 +65,46 @@ def bicubic_interpolator(topography):
     return wrapped_bicubic
 
 def interpolate_fourier(topography, nb_grid_pts):
-    """
-    Interpolates the
+    r"""
+    Interpolates the heights of `topography` on a finer grid given by `nb_grid_pts`
+    by padding the fourier spectrum with zeros.
+
+    Only 2D implemented.
+
+    Note: at the nyquist frequenca we use the "minimal-oscillation" interpolation,
+    i.e. we assume a cosinus wave (https://math.mit.edu/~stevenj/fft-deriv.pdf).
 
     Parameters
     ----------
-    topography
-    nb_grid_pts
+    topography: Topography
+    nb_grid_pts: tuple
 
     Returns
     -------
+    Topography with interolated values
 
     """
-    bigspectrum = np.zeros((nb_grid_pts[0], nb_grid_pts[1]//2 + 1),
-                          dtype=complex)
+    bigspectrum = np.zeros((nb_grid_pts[0], nb_grid_pts[1] // 2 + 1),
+                           dtype=complex)
     smallspectrum = np.fft.rfft2(topography.heights())
     snx, sny = smallspectrum.shape
 
-    i =  snx//2 if snx %2 == 0 else (snx -1) //2 +1
+    i = snx // 2 if snx % 2 == 0 else (snx - 1) // 2 + 1
     bigspectrum[:i, :sny] = smallspectrum[:i, :sny]
-    bigspectrum[-(snx-i):, :sny] = smallspectrum[-(snx-i):, :sny]
+    bigspectrum[-(snx - i):, :sny] = smallspectrum[-(snx - i):, :sny]
+
+    # in the y direction, the entries at the nyquist frequency are automatically
+    # symmetrised by the rfft, corresponding to a cosinusoidal wave, i.e. the
+    # "minimal-oscillation" trigonometric interpolation
+    # (https://math.mit.edu/~stevenj/fft-deriv.pdf).
+    # To ensure the same behaviour in the x direction
+    # (see test_fourier_interpolate_transpose_symmetry`)`
+    # we have to mirror the entries at the niquist frequency by hand
+    if snx%2 == 0:
+        bigspectrum[i, :sny] = smallspectrum[i, :sny]
 
     return Topography(np.fft.irfft2(bigspectrum, s=nb_grid_pts)
-                      * np.prod(nb_grid_pts) / np.prod(topography.nb_grid_pts), # normalization
+                      * np.prod(nb_grid_pts) / np.prod(topography.nb_grid_pts),  # normalization
                       physical_sizes=topography.physical_sizes)
 
 Topography.register_function("interpolate_bicubic", bicubic_interpolator)
