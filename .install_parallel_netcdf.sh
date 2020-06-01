001b18b915be1627da91cde82e44785ddbb6dbc0
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2020-06-01 21:21:00 +0200

    CI: Ignore certificate in curl

--- .install_parallel_netcdf.sh
+++ .install_parallel_netcdf.sh
@@ -4,14 +4,14 @@
 # broken on most distributions.
 #
 
-curl https://parallel-netcdf.github.io/Release/pnetcdf-${PNETCDF_VERSION}.tar.gz | tar -xzC ${BUILDDIR} &&
+curl -k https://parallel-netcdf.github.io/Release/pnetcdf-${PNETCDF_VERSION}.tar.gz | tar -xzC ${BUILDDIR} &&
   cd ${BUILDDIR}/pnetcdf-${PNETCDF_VERSION} &&
   CC=mpicc CXX=mpicxx ./configure --disable-fortran --disable-cxx --enable-shared --prefix=${PREFIX} &&
   make &&
   make install &&
   cd -
 
-curl https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz | tar -xzC ${BUILDDIR} &&
+curl -k https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz | tar -xzC ${BUILDDIR} &&
   cd ${BUILDDIR}/hdf5-${HDF5_VERSION} &&
   CC=mpicc CXX=mpicxx ./configure --enable-parallel --prefix=${PREFIX} &&
   make &&
@@ -20,7 +20,7 @@ curl https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-${HDF5_VERSIO
 
 # We need to compile NetCDF ourselves because there is no package that has
 # parallel PnetCDF and HDF5 enabled.
-curl https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-c-${NETCDF4_VERSION}.tar.gz | tar -xzC ${BUILDDIR} &&
+curl -k https://www.unidata.ucar.edu/downloads/netcdf/ftp/netcdf-c-${NETCDF4_VERSION}.tar.gz | tar -xzC ${BUILDDIR} &&
   mkdir ${BUILDDIR}/netcdf-c-build &&
   cd ${BUILDDIR}/netcdf-c-build &&
   cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx -DUSE_PARALLEL=ON -DENABLE_PARALLEL4=ON -DENABLE_PNETCDF=ON ${BUILDDIR}/netcdf-c-${NETCDF4_VERSION} &&
