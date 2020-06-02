035eae25bb8b66de92407d8a1df48834d4d74ca1
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2020-06-02 21:31:39 +0200

    TST: Obtain domain decomposition from muFFT directly

--- tests/IO/test_netcdf.py
+++ tests/IO/test_netcdf.py
@@ -26,18 +26,16 @@
 import os
 
 import numpy as np
-import pytest
 
 from netCDF4 import Dataset
 from NuMPI import MPI
+from muFFT import FFT
 
-# TODO(pastewka): test_save_and_load should become independent of ContactMechanics
-# from PyCo.ContactMechanics import PeriodicFFTElasticHalfSpace
 from SurfaceTopography.IO import read_topography
 from SurfaceTopography.IO.NC import NCReader
 from SurfaceTopography.Generation import fourier_synthesis
 
-@pytest.mark.skip(reason='depends on FFTElasticHalfSpace')
+
 def test_save_and_load(comm):
     nb_grid_pts = (128, 128)
     size = (3, 3)
@@ -46,10 +44,9 @@ def test_save_and_load(comm):
     t = fourier_synthesis(nb_grid_pts, size, 0.8, rms_slope=0.1)
     t.info['unit'] = 'μm'
 
-    substrate = PeriodicFFTElasticHalfSpace(nb_grid_pts, 1,
-                                            fft='serial' if comm is None or comm.size == 1 else 'mpi',
-                                            communicator=comm)
-    dt = t.domain_decompose(substrate.subdomain_locations, substrate.nb_subdomain_grid_pts,
+    fft = FFT(nb_grid_pts, 1, communicator=comm)
+    dt = t.domain_decompose(fft.subdomain_locations,
+                            fft.nb_subdomain_grid_pts,
                             communicator=comm)
     assert t.info['unit'] == 'μm'
     if comm.size > 1:
@@ -70,8 +67,8 @@ def test_save_and_load(comm):
 
     assert r.channels[0].nb_grid_pts == nb_grid_pts
 
-    t3 = r.topography(subdomain_locations=substrate.subdomain_locations,
-                      nb_subdomain_grid_pts=substrate.nb_subdomain_grid_pts)
+    t3 = r.topography(subdomain_locations=fft.subdomain_locations,
+                      nb_subdomain_grid_pts=fft.nb_subdomain_grid_pts)
 
     assert t.physical_sizes == t3.physical_sizes
     assert t.info['unit'] == t3.info['unit']
@@ -82,6 +79,7 @@ def test_save_and_load(comm):
     comm.barrier()
     if comm.rank == 0:
         os.remove('parallel_save_test.nc')
+
 
 def test_save_and_load_no_unit(comm_self):
     nb_grid_pts = (128, 128)
@@ -101,6 +99,7 @@ def test_save_and_load_no_unit(comm_self):
 
     os.remove('no_unit.nc')
 
+
 def test_load_no_physical_sizes(comm_self):
     nb_grid_pts = (128, 128)
     size = (3, 3)
@@ -108,15 +107,17 @@ def test_load_no_physical_sizes(comm_self):
     np.random.seed(1)
     t = fourier_synthesis(nb_grid_pts, size, 0.8, rms_slope=0.1)
 
-    # Topographies always have physical size information, we need to create a NetCDF file without any manually
-    with Dataset('no_physical_sizes.nc', 'w', format='NETCDF3_CLASSIC') as nc:
+    # Topographies always have physical size information, we need to create a
+    # NetCDF file without any manually
+    with Dataset('no_physical_sizes.nc', 'w',
+                 format='NETCDF3_CLASSIC') as nc:
         nc.createDimension('x', nb_grid_pts[0])
         nc.createDimension('y', nb_grid_pts[1])
         nc.createVariable('heights', 'f8', ('x', 'y'))
         nc.variables['heights'] = t.heights()
 
     # Attempt to open full file on each process
-    #with pytest.raises(ValueError):
+    # with pytest.raises(ValueError):
     #    # This raises an error because the physical sizes are not present
     #    t2 = read_topography('no_physical_sizes.nc')
     t2 = read_topography('no_physical_sizes.nc', physical_sizes=size)
@@ -126,6 +127,7 @@ def test_load_no_physical_sizes(comm_self):
     np.testing.assert_array_almost_equal(t.heights(), t2.heights())
 
     os.remove('no_physical_sizes.nc')
+
 
 if __name__ == '__main__':
     test_save_and_load(MPI.COMM_WORLD)
