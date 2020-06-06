0b7e84b92975a7538cd0b8bf63cc3c6c7fffbfdb
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-06-06 07:15:37 +0200

    DOC: closes #7. Rename parameters and enhance docstring for highcut and lowcut

--- SurfaceTopography/Uniform/Filtering.py
+++ SurfaceTopography/Uniform/Filtering.py
@@ -27,22 +27,38 @@ from SurfaceTopography import Topography
 import numpy as np
 
 
-def highcut(topography, q_s=None, lam_s=None, kind="circular step"):
-    """
+def highcut(topography, cutoff_wavevector=None, cutoff_wavelength=None,
+            kind="circular step"):
+    r"""Applies a highcut filter to the topography using fft.
+
+    for `kind=="circular step"` (default), parts of the spectrum with
+    `|q| > cutoff_wavevector` are set to zero
+
+    for `kind=="square step"`, parts of the spectrum with
+    `q_x > cutoff_wavevector or q_y > cutoff_wavevector ` are set to zero
+
+    either `cutoff_wavelength` or
+    `cutoff_wavevector` :math:`= 2 pi /` `cutoff_wavelength`
+    have to be provided.
 
     Parameters
     ----------
-    topography: SurfaceTopography
-    q_s: float
+    topography: Topography
+    cutoff_wavevector: float
     highest wavevector
-    lam_s: float
+    cutoff_wavelength: float
     shortest wavelength
     kind: {"circular step", "square step"}
-    ax: Axes, optional
 
     Returns
     -------
-    SurfaceTopography with filtered heights
+    Topography with filtered heights
+
+    Examples
+    --------
+    >>> highcut(topography, cutoff_wavevector=2 * np.pi / l)
+    >>> highcut(topography, cutoff_wavelength=l) # equivalent
+
     """
 
     if not topography.is_periodic:
@@ -64,43 +80,58 @@ def highcut(topography, q_s=None, lam_s=None, kind="circular step"):
     print(q2.shape)
     # square of the norm of the wavevector
 
-    if q_s is None:
-        if lam_s is not None:
-            q_s = 2 * np.pi / lam_s
+    if cutoff_wavevector is None:
+        if cutoff_wavelength is not None:
+            cutoff_wavevector = 2 * np.pi / cutoff_wavelength
         else:
             raise ValueError("q_s or lam_s should be provided")
-    elif lam_s is not None:
+    elif cutoff_wavelength is not None:
         raise ValueError("q_s or lam_s should be provided")
 
     filt = np.ones_like(q2)
 
     if kind == "circular step":
-        filt *= (q2 <= q_s ** 2)
+        filt *= (q2 <= cutoff_wavevector ** 2)
     elif kind == "square step":
-        filt *= (np.abs(qx.reshape(-1, 1)) <= q_s) * (
-                    np.abs(qy.reshape(1, -1)) <= q_s)
+        filt *= (np.abs(qx.reshape(-1, 1)) <= cutoff_wavevector) * (
+                np.abs(qy.reshape(1, -1)) <= cutoff_wavevector)
 
     h_qs = np.fft.irfftn(np.fft.rfftn(topography.heights()) * filt)
 
     return Topography(h_qs, physical_sizes=topography.physical_sizes)
 
 
-def lowcut(topography, q_l=None, lam_l=None, kind="circular step"):
-    """
+def lowcut(topography, cutoff_wavevector=None, cutoff_wavelength=None, kind="circular step"):
+    r"""Applies a lowcut filter to the topography using fft.
+
+    for `kind=="circular step"` (default), parts of the spectrum with
+    `|q| < cutoff_wavevector` are set to zero
+
+    for `kind=="square step"`, parts of the spectrum with
+    `q_x < cutoff_wavevector or q_y < cutoff_wavevector ` are set to zero
+
+    either `cutoff_wavelength` or
+    `cutoff_wavevector` :math:`= 2 pi /` `cutoff_wavelength`
+    have to be provided.
 
     Parameters
     ----------
-    topography: SurfaceTopography
-    q_s: float
+    topography: Topography
+    cutoff_wavevector: float
     highest wavevector
-    lam_s: float
+    cutoff_wavelength: float
     shortest wavelength
     kind: {"circular step", "square step"}
-    ax: Axes, optional
 
     Returns
     -------
-    SurfaceTopography with filtered heights
+    Topography with filtered heights
+
+    Examples
+    --------
+    >>> lowcut(topography, cutoff_wavevector=2 * np.pi / l)
+    >>> lowcut(topography, cutoff_wavelength=l) # equivalent
+
     """
 
     if not topography.is_periodic:
@@ -122,21 +153,21 @@ def lowcut(topography, q_l=None, lam_l=None, kind="circular step"):
     print(q2.shape)
     # square of the norm of the wavevector
 
-    if q_l is None:
-        if lam_l is not None:
-            q_l = 2 * np.pi / lam_l
+    if cutoff_wavevector is None:
+        if cutoff_wavelength is not None:
+            cutoff_wavevector = 2 * np.pi / cutoff_wavelength
         else:
-            raise ValueError("q_l or lam_l should be provided")
-    elif lam_l is not None:
-        raise ValueError("q_l or lam_l should be provided")
+            raise ValueError("cutoff_wavevector or cutoff_wavelength should be provided")
+    elif cutoff_wavelength is not None:
+        raise ValueError("cutoff_wavevector or cutoff_wavelength should be provided")
 
     filt = np.ones_like(q2)
 
     if kind == "circular step":
-        filt *= (q2 >= q_l ** 2)
+        filt *= (q2 >= cutoff_wavevector ** 2)
     elif kind == "square step":
-        filt *= (np.abs(qx.reshape(-1, 1)) >= q_l) * (
-                    np.abs(qy.reshape(1, -1)) >= q_l)
+        filt *= (np.abs(qx.reshape(-1, 1)) >= cutoff_wavevector) * (
+                    np.abs(qy.reshape(1, -1)) >= cutoff_wavevector)
 
     h_qs = np.fft.irfftn(np.fft.rfftn(topography.heights()) * filt)
 
@@ -144,17 +175,24 @@ def lowcut(topography, q_l=None, lam_l=None, kind="circular step"):
 
 
 def isotropic_filter(topography, filter_function=lambda q: np.exp(-q)):
-    r"""
-    Multiplies filter_function(q) to the spectrum of the topography
+    r"""Multiplies filter_function(|q|) to the spectrum of the topography
+    (q is the wavevector)
+
+    returns
+
+    ..math :: h^f_{ij} = FFT^-1(f(|q_{kl}|) FFT(h)_{kl})_{ij}
+
+    with :math:`f` the `filter_function`
 
     Parameters
     ----------
-    topography: SurfaceTopography object
-    filter_function: function of the absolute value of the wavevector |q|
+    topography: Topography
+    filter_function:
+    function of the absolute value of the wavevector |q|
 
     Returns
     -------
-    SurfaceTopography with the modified heights
+    Topography with the modified heights
     """
 
     if not topography.is_periodic:
