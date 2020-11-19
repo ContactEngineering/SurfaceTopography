BUG: Only reuse array if i == 0; Fourier derivative test now compares to central differences

SurfaceTopography/Uniform/common.py
@@ -171,16 +171,7 @@ def derivative(topography, n, operator=None, periodic=None):
             'this is required to be able to compute a derivative.')
 
     if operator is None:
-        if n is None:
-            raise ValueError('Please specify either *n* (order of derivative) '
-                             'or *operator* (muFFT derivative operator). You '
-                             'specified neither.')
-
         operator = _get_default_derivative_operator(n, topography.dim)
-    elif n is not None:
-        raise ValueError('Please specify either *n* (order of derivative) or '
-                         '*operator* (muFFT derivative operator). You '
-                         'specified both.')
 
     grid_spacing = topography.pixel_size
     is_periodic = topography.is_periodic if periodic is None else periodic
@@ -200,10 +191,7 @@ def derivative(topography, n, operator=None, periodic=None):
         fourier_copy = fourier_array.copy()
         der = []
         for i, op in enumerate(operator):
-            if i != 0:
-                fourier_array *= op.fourier(fft.fftfreq)
-            else:
-                fourier_array[...] = fourier_copy * op.fourier(fft.fftfreq)
+            fourier_array[...] = fourier_copy * op.fourier(fft.fftfreq)
             fft.ifft(fourier_field, real_field)
             _der = np.array(real_field, copy=False) * fft.normalisation / grid_spacing[i] ** n
             if not is_periodic:

test/test_derivatives.py
@@ -4,6 +4,8 @@ Test derivatives
 
 import numpy as np
 
+import muFFT.Stencils2D as Stencils2D
+
 from SurfaceTopography import UniformLineScan
 from SurfaceTopography.Generation import fourier_synthesis
 
@@ -55,12 +57,15 @@ def test_fourier_derivative(plot=False):
 
     lc = 0.5
     topography = fourier_synthesis((nx, ny), (sx, sy), 0.8, rms_height=1.,
-                                   short_cutoff=lc, long_cutoff=lc + 1e-9, )
+                                   short_cutoff=lc, long_cutoff=lc + 1e-9)
     topography = topography.scale(1 / topography.rms_height())
 
-    # numerical derivatives to double check
+    # Fourier derivative
     dx, dy = topography.fourier_derivative(imtol=1e-12)
-    dx_num, dy_num = topography.derivative(1)
+
+    # Finite-differences. We use central differences because this produces the
+    # derivative at the same point as the Fourier derivative
+    dx_num, dy_num = topography.derivative(1, operator=Stencils2D.central)
 
     np.testing.assert_allclose(dx, dx_num, atol=topography.rms_slope() * 1e-1)
     np.testing.assert_allclose(dy, dy_num, atol=topography.rms_slope() * 1e-1)