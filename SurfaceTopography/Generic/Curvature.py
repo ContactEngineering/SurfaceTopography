45b06b2945ed81e50ddefbb45437a296c08a1727
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-03-23 20:39:02 +0100

    WIP: Scale-dependent curvature

--- SurfaceTopography/Generic/Curvature.py
+++ SurfaceTopography/Generic/Curvature.py
@@ -28,6 +28,7 @@ Scale-dependent slope
 
 import numpy as np
 
+from ..common import radial_average
 from ..HeightContainer import UniformTopographyInterface
 from ..HeightContainer import NonuniformLineScanInterface
 
@@ -63,11 +64,13 @@ def scale_dependent_curvature_1D(topography, **kwargs):
     """  # noqa: E501
     r, A = topography.autocorrelation_1D(**kwargs)
     n = (len(r) + 1) // 2
+    r = r[1:n]
+    B = 8 * A[1:n] - 2 * A[2::2]
     # Important: The following expression relies on the fact that r is equally spaced!
-    return r[1:n], np.sqrt(8 * A[1:n] - 2 * A[2::2]) / r[1:n]
+    return r[B > 0], np.sqrt(B[B > 0]) / r[B > 0] ** 2
 
 
-def scale_dependent_curvature_2D(topography, **kwargs):
+def scale_dependent_curvature_2D(topography, nbins=100):
     r"""
     Compute the two-dimensional, radially averaged scale-dependent slope.
 
@@ -86,8 +89,9 @@ def scale_dependent_curvature_2D(topography, **kwargs):
     ----------
     topography : :obj:`SurfaceTopography` or :obj:`UniformLineScan`
         Container storing the uniform topography map
-    **kwargs : dict
-        Additional keyword parameters are passed on to `autocorrelation_2D`
+    nbins : int
+        Number of bins for radial average. Note: Returned array can be smaller
+        than this because bins without data points are discarded.
 
     Returns
     -------
@@ -96,9 +100,19 @@ def scale_dependent_curvature_2D(topography, **kwargs):
     slope : array
         Slope. (Units: dimensionless)
     """  # noqa: E501
-    r, A = topography.autocorrelation_2D(**kwargs)
-    n = (len(r) + 1) // 2
-    return r[1:n], np.sqrt(8 * A[1:n] - 2 * A[2::2]) / r[1:n]
+    _, _, A_xy = topography.autocorrelation_2D(return_map=True)
+    nx, ny = A_xy.shape
+    nx = (nx + 1) // 2
+    ny = (ny + 1) // 2
+
+    sx, sy = topography.physical_sizes
+    B_xy = 8 * A_xy[1:nx, 1:ny] - 2 * A_xy[2::2, 2::2]
+    # Radial average
+    r_edges, n, r_val, B_val = radial_average(
+        # pylint: disable=invalid-name
+        B_xy, (sx + sy) / 2, nbins, physical_sizes=(sx, sy), full=False)
+
+    return r_val[B_val > 0], np.sqrt(B_val[B_val > 0]) / r_val[B_val > 0]
 
 # Register analysis functions from this module
 UniformTopographyInterface.register_function('scale_dependent_curvature_1D',
@@ -107,5 +121,3 @@ NonuniformLineScanInterface.register_function('scale_dependent_curvature_1D',
                                               scale_dependent_curvature_1D)
 UniformTopographyInterface.register_function('scale_dependent_curvature_2D',
                                              scale_dependent_curvature_2D)
-NonuniformLineScanInterface.register_function('scale_dependent_curvature_2D',
-                                              scale_dependent_curvature_2D)
