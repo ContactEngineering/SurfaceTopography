BUG: Topography was loaded in wrong order

SurfaceTopography/IO/ZON.py
@@ -139,8 +139,7 @@ This reader open ZON files that are written by some Keyence instruments.
             with ZipFile(f, 'r') as z:
                 with z.open(channel_info.info['data_uuid']) as f:
                     f.read(16)  # skip header
-                    height_data = np.frombuffer(f.read(4 * nx * ny), np.dtype('i4'))
-                    height_data.shape = (nx, ny)
+                    height_data = np.frombuffer(f.read(4 * nx * ny), np.dtype('i4')).reshape((ny, nx)).T
         finally:
             if not already_open:
                 f.close()

test/IO/test_zon.py
@@ -67,9 +67,9 @@ def test_topography():
 
     topography = loader.topography()
 
-    print(topography.heights()[0, 0])
     # Check one height value
     np.testing.assert_almost_equal(topography.heights()[0, 0], 1.301e-05)
+    np.testing.assert_almost_equal(topography.heights()[10, 5], 8.47e-05)
 
     # Check the value of one of the metadata
     assert topography.info['unit'] == 'm'