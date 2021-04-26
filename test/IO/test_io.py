471f03a09183e9624db29e78c2e1468b5b617fb4
Michael Röttger <info@michael-roettger.de>
2021-04-26 17:37:23 +0200

    TST: Fixed PEP 8 issue.

--- test/IO/test_io.py
+++ test/IO/test_io.py
@@ -203,36 +203,36 @@ def test_is_binary_stream():
 
 @pytest.mark.parametrize('fn', text_example_file_list + text_example_without_size_file_list + binary_example_file_list)
 def test_can_be_pickled(fn):
-        reader = open_topography(fn)
-        physical_sizes = None
-        if reader.default_channel.dim != 1:
-            physical_sizes = reader.default_channel.physical_sizes \
-                if reader.default_channel.physical_sizes is not None \
-                else (1.,) * reader.default_channel.dim
+    reader = open_topography(fn)
+    physical_sizes = None
+    if reader.default_channel.dim != 1:
+        physical_sizes = reader.default_channel.physical_sizes \
+            if reader.default_channel.physical_sizes is not None \
+            else (1.,) * reader.default_channel.dim
 
-        topography = reader.topography(physical_sizes=physical_sizes)
-        topographies = [topography]
-        if hasattr(topography, 'to_uniform'):
-            topographies += [topography.to_uniform(100, 0)]
-        for t in topographies:
-            s = pickle.dumps(t)
-            pickled_t = pickle.loads(s)
+    topography = reader.topography(physical_sizes=physical_sizes)
+    topographies = [topography]
+    if hasattr(topography, 'to_uniform'):
+        topographies += [topography.to_uniform(100, 0)]
+    for t in topographies:
+        s = pickle.dumps(t)
+        pickled_t = pickle.loads(s)
 
-            #
-            # Compare some attributes after unpickling
-            #
-            # sometimes the result is a list of topographies
-            multiple = isinstance(t, list)
-            if not multiple:
-                t = [t]
-                pickled_t = [pickled_t]
+        #
+        # Compare some attributes after unpickling
+        #
+        # sometimes the result is a list of topographies
+        multiple = isinstance(t, list)
+        if not multiple:
+            t = [t]
+            pickled_t = [pickled_t]
 
-            for x, y in zip(t, pickled_t):
-                for attr in ['dim', 'physical_sizes', 'is_periodic']:
-                    assert getattr(x, attr) == getattr(y, attr)
-                if x.physical_sizes is not None:
-                    assert_array_equal(x.positions(), y.positions())
-                    assert_array_equal(x.heights(), y.heights())
+        for x, y in zip(t, pickled_t):
+            for attr in ['dim', 'physical_sizes', 'is_periodic']:
+                assert getattr(x, attr) == getattr(y, attr)
+            if x.physical_sizes is not None:
+                assert_array_equal(x.positions(), y.positions())
+                assert_array_equal(x.heights(), y.heights())
 
 
 @pytest.mark.parametrize('fn', text_example_file_list + text_example_without_size_file_list + binary_example_file_list)
