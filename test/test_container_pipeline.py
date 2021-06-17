a8fd1a681bd4b83427de57be285a77b1aef65815
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-06-17 21:04:45 +0200

    TST: Added test

--- test/test_container_pipeline.py
+++ test/test_container_pipeline.py
@@ -12,4 +12,4 @@ from SurfaceTopography import read_published_container
 def test_scale_dependent_statistical_property():
     c, = read_published_container('https://contact.engineering/go/867nv/')
     s = c.scale_dependent_statistical_property(lambda x, y: np.var(x), n=1, distance=[0.01, 0.1, 1.0, 10], unit='um')
-    print(s)
+    assert (np.diff(s) < 0).all()
