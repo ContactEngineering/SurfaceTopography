32e0508f4370601e874e5c9c316ed8c7c5103832
Michael Röttger <info@michael-roettger.de>
2021-06-18 17:50:17 +0200

    TST: Removed comma which broke test.

--- test/test_container_pipeline.py
+++ test/test_container_pipeline.py
@@ -7,6 +7,6 @@ import numpy as np
 from SurfaceTopography import read_container
 
 def test_scale_dependent_statistical_property(file_format_examples):
-    c, = read_container(f'{file_format_examples}/container1.zip'),
+    c, = read_container(f'{file_format_examples}/container1.zip')
     s = c.scale_dependent_statistical_property(lambda x, y: np.var(x), n=1, distance=[0.01, 0.1, 1.0, 10], unit='um')
     assert (np.diff(s) < 0).all()
