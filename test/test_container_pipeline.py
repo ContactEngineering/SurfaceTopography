315c4ca7f1e2a51d01d558be232e6d00813f7f96
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-06-18 16:25:51 +0200

    MAINT: Use local copy, not published container

--- test/test_container_pipeline.py
+++ test/test_container_pipeline.py
@@ -7,9 +7,9 @@ import pytest
 
 import numpy as np
 
-from SurfaceTopography import read_published_container
+from SurfaceTopography import read_container
 
-def test_scale_dependent_statistical_property():
-    c, = read_published_container('https://contact.engineering/go/867nv/')
+def test_scale_dependent_statistical_property(file_format_examples):
+    c, = read_container(f'{file_format_examples}/container1.zip'),
     s = c.scale_dependent_statistical_property(lambda x, y: np.var(x), n=1, distance=[0.01, 0.1, 1.0, 10], unit='um')
     assert (np.diff(s) < 0).all()
