a067e8c0f61b6604d880fb15bacd17ebca66957d
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-06-29 10:35:28 +0200

    CI: Need to explicitly specify NetCDF path

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install libfftw3-dev libopenblas-dev python3-numpy python3-scipy
+sudo apt-get install libfftw3-dev libopenblas-dev
 if [ "$WITH_MPI" == "yes" ]; then
   sudo apt-get install openmpi-bin libopenmpi-dev libfftw3-mpi-dev libhdf5-mpi-dev libnetcdf-mpi-dev
 fi
