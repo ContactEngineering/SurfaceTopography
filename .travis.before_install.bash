eb8f4a0d140db14273120d08178205dd8a74136d
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-06-29 11:11:36 +0200

    CI: Install mpi4py before netCDF4 - otherwise parallel is disabled

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get install libfftw3-dev libopenblas-dev
 if [ "$WITH_MPI" == "yes" ]; then
-  sudo apt-get install openmpi-bin libopenmpi-dev libfftw3-mpi-dev libhdf5-mpi-dev libnetcdf-mpi-dev
+  sudo apt-get install openmpi-bin libopenmpi-dev libfftw3-mpi-dev libhdf5-mpi-dev libnetcdf-pnetcdf-dev
 fi
 #wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 #bash miniconda.sh -b -p $HOME/miniconda3
@@ -16,6 +16,6 @@ fi
 #source activate test-environment
 python -m pip install $(grep numpy requirements.txt)
 if [ "$WITH_MPI" == "yes" ]; then
-  python -m pip install --no-binary mpi4py mpi4py==${MPI4PY_VERSION}
+  python -m pip install --force-reinstall --no-binary mpi4py mpi4py==${MPI4PY_VERSION}
   #BUILDDIR=/tmp PREFIX=$HOME/.local source .install_parallel_netcdf.sh
 fi
