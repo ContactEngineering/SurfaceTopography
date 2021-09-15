MAINT: Unified scale-dependent statistical properties calculation between uniform and nonuniform topographies by moving the interpolation into the computation of the derivative BUG: Fixed computation of reliability cutoff for nonuniform line scans

SurfaceTopography/Generic/ScaleDependentStatistics.py
@@ -23,12 +23,11 @@
 # SOFTWARE.
 #
 
-from ..common import toiter, fromiter
-from ..HeightContainer import NonuniformLineScanInterface
+from ..HeightContainer import NonuniformLineScanInterface, UniformTopographyInterface
 
 
-def scale_dependent_statistical_property(self, func, n, distance, interpolation='linear',
-                                         progress_callback=None):
+def scale_dependent_statistical_property(topography, func, n=1, scale_factor=None, distance=None,
+                                         interpolation='linear', progress_callback=None):
     """
     Compute statistical properties of a uniform topography at specific scales.
     The scale is specified either by `scale_factors` or `distance`. These
@@ -42,8 +41,8 @@ def scale_dependent_statistical_property(self, func, n, distance, interpolation=
 
     Parameters
     ----------
-    self : :class:`SurfaceTopography.NonuniformLineScan`
-        Topography or line scan.
+    topography : Topography or UniformLineScan
+        Topogaphy or line scan.
     func : callable
         The function that computes the statistical properties:
 
@@ -56,14 +55,23 @@ def scale_dependent_statistical_property(self, func, n, distance, interpolation=
         return a scalar value or an array, but the array size must be fixed.
     n : int, optional
         Order of derivative. (Default: 1)
+    scale_factor : float or np.ndarray
+        Scale factor for rescaling the finite differences stencil. A scale
+        factor of unity means the derivative is computed at the size of the
+        individual pixel.
     distance : float or np.ndarray
         Characteristic distances at which the derivatives are computed. If
         this is an array, then the statistical property is computed at each
         of these distances.
+    unit : str
+        Unit of the distance array. All topographies are converted to this
+        unit before the derivative is computed.
     interpolation : str, optional
-        Interpolation method to use for computing derivatives. Can only be
-        'linear' for nonuniform line scans and is provided for compatibility
-        with the corresponding keyword parameter of the uniform containers.
+        Interpolation method to use for computing derivatives at distances
+        that do not equal an integer multiple of the grid spacing. Use
+        'linear' for a local liner interpolation or 'fourier' for global
+        Fourier interpolation. Note that Fourier interpolation carries large
+        errors for nonperiodic topographies and should be used with care.
         (Default: 'linear')
     progress_callback : func, optional
         Function taking iteration and the total number of iterations as
@@ -83,21 +91,29 @@ def scale_dependent_statistical_property(self, func, n, distance, interpolation=
     >>> s = t.scale_dependent_statistical_property(lambda x, y=None: np.var(x), distance=distances[1::20])
     >>> np.testing.assert_allclose(2 * A[1::20] / distances[1::20] ** 2, s)
     """
-    if interpolation != 'linear':
-        raise ValueError('Only linear interpolation is support for scale-dependent statistical properties of '
-                         'nonuniform line scans.')
-    stat = []
-    distances = toiter(distance)
-    for i, d in enumerate(distances):
-        if progress_callback is not None:
-            progress_callback(i, len(distances))
-        stat += self.to_uniform(pixel_size=d / n).scale_dependent_statistical_property(func=func, n=n, scale_factor=1,
-                                                                                       interpolation='disable')
-    if progress_callback is not None:
-        progress_callback(len(distances), len(distances))
+    d = topography.derivative(n=n, scale_factor=scale_factor, distance=distance, interpolation=interpolation,
+                              progress_callback=progress_callback)
+    if topography.dim == 1:
+        try:
+            if scale_factor is not None:
+                iter(scale_factor)
+            if distance is not None:
+                iter(distance)
+            return [func(_d) for _d in d]
+        except TypeError:
+            return func(d)
+    else:
+        try:
+            if scale_factor is not None:
+                iter(scale_factor)
+            if distance is not None:
+                iter(distance)
+            return [func(dx, dy) for dx, dy in zip(*d)]
+        except TypeError:
+            return func(*d)
 
-    return fromiter(stat, distance)
 
-
-NonuniformLineScanInterface.register_function('scale_dependent_statistical_property',
-                                              scale_dependent_statistical_property)
+UniformTopographyInterface.register_function(
+    'scale_dependent_statistical_property', scale_dependent_statistical_property)
+NonuniformLineScanInterface.register_function(
+    'scale_dependent_statistical_property', scale_dependent_statistical_property)

SurfaceTopography/Generic/ScanningProbe.py
@@ -57,17 +57,8 @@ def scanning_probe_reliability_cutoff(self, tip_radius, safety_factor=1 / 2):
     """
     target_curvature = safety_factor / tip_radius
 
-    if self.dim == 1:
-        sx, = self.physical_sizes
-        px, = self.pixel_size
-    elif self.dim == 2:
-        sx, sy = self.physical_sizes
-        px, py = self.pixel_size
-    else:
-        raise ValueError(f"Don't know how to handle a topography of dimension {self.dim}.")
-
-    def objective(scale_factor):
-        d = self.derivative(n=2, scale_factor=scale_factor)
+    def objective(distance):
+        d = self.derivative(n=2, distance=distance)
         if self.dim == 1:
             return target_curvature + np.min(d)
         elif self.dim == 2:
@@ -75,10 +66,10 @@ def scanning_probe_reliability_cutoff(self, tip_radius, safety_factor=1 / 2):
         else:
             raise ValueError(f'Cannot handle a {self.dim}-dimensional topography.')
 
-    reliability_cutoff = 2 * scipy.optimize.brentq(objective,
-                                                   1, sx / 4 / px,  # bounds
-                                                   xtol=1e-4
-                                                   ) * px
+    lower, upper = self.bandwidth()
+    reliability_cutoff = scipy.optimize.brentq(objective,
+                                               2 * lower, upper / 2,  # bounds
+                                               xtol=1e-4)
 
     return reliability_cutoff
 

SurfaceTopography/Uniform/Derivative.py
@@ -240,11 +240,16 @@ def derivative(self, n, scale_factor=None, distance=None, operator=None, periodi
             # Convert distance to scale factor
             if self.dim == 1:
                 px, = pixel_size
-                scale_factor = [d / (n * px) for d in toiter(distance)]
+                try:
+                    scale_factor = [d / (n * px) for d in distance]
+                except TypeError:
+                    scale_factor = distance / (n * px)
             else:
                 px, py = pixel_size
-                scale_factor = np.array([(d / (n * px), d / (n * py)) for d in toiter(distance)])
-
+                try:
+                    scale_factor = np.array([(d / (n * px), d / (n * py)) for d in distance])
+                except TypeError:
+                    scale_factor = (distance / (n * px), distance / (n * py))
     elif distance is not None:
         raise ValueError('Please specify either `scale_factor` or `distance`')
 

test/test_reliability_cutoff.py
@@ -80,4 +80,8 @@ def test_reliability_cutoff_line_scan(file_format_examples):
         }
     })
     cut = surf.short_reliability_cutoff()
-    np.testing.assert_allclose(cut, 0.126519)
+    np.testing.assert_allclose(cut, 0.126504, atol=1e-6)
+
+    cut = surf.to_nonuniform().short_reliability_cutoff()
+    # This differs from the above because the derivatives are computed at slightly different locations
+    np.testing.assert_allclose(cut, 0.126505, atol=1e-6)