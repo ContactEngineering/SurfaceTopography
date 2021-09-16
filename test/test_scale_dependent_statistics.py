180ac7fa53321a73f7a57f2ba21556520d173ae4
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-09-16 20:54:34 +0200

    MAINT: Satisfy flake8

--- test/test_scale_dependent_statistics.py
+++ test/test_scale_dependent_statistics.py
@@ -74,8 +74,8 @@ def test_container_uniform(file_format_examples):
 
     # Test that specifying distances where no data exists does not raise an exception
     iterations = []
-    _, s = c.scale_dependent_statistical_property(lambda x, y: np.var(x), n=1, distance=[0.00001, 1.0, 10000], unit='um',
-                                                  progress_callback=lambda i, n: iterations.append(i))
+    _, s = c.scale_dependent_statistical_property(lambda x, y: np.var(x), n=1, distance=[0.00001, 1.0, 10000],
+                                                  unit='um', progress_callback=lambda i, n: iterations.append(i))
     assert s[0] is None
     assert s[2] is None
     np.testing.assert_allclose(np.array(iterations), np.arange(len(c) + 1))
