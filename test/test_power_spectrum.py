fe03dc2e327fcab3eb81430232f00dfdab6318ae
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-11-22 23:51:12 +0100

    MAINT: Satisfy flake8

--- test/test_power_spectrum.py
+++ test/test_power_spectrum.py
@@ -333,7 +333,8 @@ def test_reliability_cutoff():
 
     # 1D
     q1, C1 = surf.power_spectrum_from_profile(reliable=True)
-    assert dois == {'10.1016/j.apsadv.2021.100190'}  # Make sure additional calls don't contaminate our dois from the previous call
+    # Make sure additional calls don't contaminate our dois from the previous call
+    assert dois == {'10.1016/j.apsadv.2021.100190'}
     q2, C2 = surf.power_spectrum_from_profile(reliable=False)
     assert len(q1) < len(q2)
     assert np.nanmax(q1) < np.nanmax(q2)
