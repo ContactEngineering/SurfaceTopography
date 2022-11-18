8f5603161f71fe61e2974b64843a65d554b23161
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-11-18 15:02:26 +0100

    MAINT: Removed unittest for X3P (switched to pytest)

--- test/IO/test_x3p.py
+++ test/IO/test_x3p.py
@@ -26,7 +26,6 @@
 
 import os
 import pytest
-import unittest
 
 import numpy as np
 
@@ -38,39 +37,31 @@ pytestmark = pytest.mark.skipif(
     MPI.COMM_WORLD.Get_size() > 1,
     reason="tests only serial functionalities, please execute with pytest")
 
-DATADIR = os.path.join(
-    os.path.dirname(
-        os.path.dirname(os.path.realpath(__file__))),
-    'file_format_examples')
+
+def test_read(file_format_examples):
+    surface = read_x3p(os.path.join(file_format_examples, 'example.x3p'))
+    nx, ny = surface.nb_grid_pts
+    assert nx == 777
+    assert ny == 1035
+    sx, sy = surface.physical_sizes
+    np.testing.assert_almost_equal(sx, 0.00068724)
+    np.testing.assert_almost_equal(sy, 0.00051593)
+    surface = read_x3p(os.path.join(file_format_examples, 'example2.x3p'))
+    nx, ny = surface.nb_grid_pts
+    assert nx == 650
+    assert ny == 650
+    sx, sy = surface.physical_sizes
+    np.testing.assert_almost_equal(sx, 8.29767313942749e-05)
+    np.testing.assert_almost_equal(sy, 0.0002044783737930349)
+    assert surface.is_uniform
 
 
-class x3pSurfaceTest(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_read(self):
-        surface = read_x3p(os.path.join(DATADIR, 'example.x3p'))
-        nx, ny = surface.nb_grid_pts
-        self.assertEqual(nx, 777)
-        self.assertEqual(ny, 1035)
-        sx, sy = surface.physical_sizes
-        self.assertAlmostEqual(sx, 0.00068724)
-        self.assertAlmostEqual(sy, 0.00051593)
-        surface = read_x3p(os.path.join(DATADIR, 'example2.x3p'))
-        nx, ny = surface.nb_grid_pts
-        self.assertEqual(nx, 650)
-        self.assertEqual(ny, 650)
-        sx, sy = surface.physical_sizes
-        self.assertAlmostEqual(sx, 8.29767313942749e-05)
-        self.assertAlmostEqual(sy, 0.0002044783737930349)
-        self.assertTrue(surface.is_uniform)
-
-    def test_points_for_uniform_topography(self):
-        surface = read_x3p(os.path.join(DATADIR, 'example.x3p'))
-        x, y, z = surface.positions_and_heights()
-        self.assertAlmostEqual(np.mean(np.diff(x[:, 0])),
-                               surface.physical_sizes[0] / surface.nb_grid_pts[
-                                   0])
-        self.assertAlmostEqual(np.mean(np.diff(y[0, :])),
-                               surface.physical_sizes[1] / surface.nb_grid_pts[
-                                   1])
+def test_points_for_uniform_topography(file_format_examples):
+    surface = read_x3p(os.path.join(file_format_examples, 'example.x3p'))
+    x, y, z = surface.positions_and_heights()
+    np.testing.assert_almost_equal(np.mean(np.diff(x[:, 0])),
+                                   surface.physical_sizes[0] / surface.nb_grid_pts[
+                                       0])
+    np.testing.assert_almost_equal(np.mean(np.diff(y[0, :])),
+                                   surface.physical_sizes[1] / surface.nb_grid_pts[
+                                       1])
