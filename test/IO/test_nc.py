ead147c40fa54e580a5b3340b419ada7f53cc55e
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-11-19 18:08:12 +0100

    MAINT: `unit` no longer part of `info` dictionary

--- test/IO/test_nc.py
+++ test/IO/test_nc.py
@@ -54,8 +54,6 @@ def test_save_and_load(comm):
                             communicator=comm)
     assert t.unit == 'µm'
     assert dt.unit == 'µm'
-    assert t.info['unit'] == 'µm'
-    assert dt.info['unit'] == 'µm'
     if comm.size > 1:
         assert dt.is_domain_decomposed
 
@@ -67,7 +65,6 @@ def test_save_and_load(comm):
 
     assert t.physical_sizes == t2.physical_sizes
     assert t.unit == t2.unit
-    assert t.info['unit'] == t2.info['unit']
     np.testing.assert_array_almost_equal(t.heights(), t2.heights())
 
     # Attempt to open file in parallel
@@ -80,7 +77,6 @@ def test_save_and_load(comm):
 
     assert t.physical_sizes == t3.physical_sizes
     assert t.unit == t3.unit
-    assert t.info['unit'] == t3.info['unit']
     np.testing.assert_array_almost_equal(dt.heights(), t3.heights())
 
     assert t3.is_periodic
@@ -151,8 +147,7 @@ def test_save_and_load_line_scan():
     size = (3,)
 
     np.random.seed(1)
-    t = fourier_synthesis(nb_grid_pts, size, 0.8, rms_slope=0.1)
-    t.info['unit'] = 'µm'
+    t = fourier_synthesis(nb_grid_pts, size, 0.8, rms_slope=0.1, unit='µm')
 
     with tempfile.TemporaryDirectory() as d:
         tmpfn = f'{d}/line_scan.nc'
