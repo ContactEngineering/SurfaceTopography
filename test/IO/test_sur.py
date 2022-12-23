398fee6520b82d73fe941986c9638e376691be28
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2022-12-23 23:32:22 +0100

    BUG: SUR reader reported wrong `height_scale_factor` in channel info

--- test/IO/test_sur.py
+++ test/IO/test_sur.py
@@ -69,3 +69,6 @@ def test_sur_metadata(file_format_examples):
     assert t.unit == 'mm'
 
     np.testing.assert_almost_equal(t.rms_height_from_area(), 0.00029098752636393403)
+
+    print(r.channels[0].height_scale_factor)
+    print(t.height_scale_factor)
