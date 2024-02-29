c992a0bb1c5b6924ee4147c231d8242886a3d86f
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-02-29 21:25:05 +0100

    ENH: O(ln N) bounds for bearing area of 2D topographic maps

--- SurfaceTopography/Uniform/BearingArea.py
+++ SurfaceTopography/Uniform/BearingArea.py
@@ -33,26 +33,48 @@ import numpy as np
 from ..HeightContainer import UniformTopographyInterface
 
 
-class Uniform1DBearingArea:
+class UniformBearingArea:
+    def bounds(self, heights):
+        """
+        Compute bounds on the bearing area for a specific height. These bounds
+        can be computed O(log(N)) time, where N is the number of elements in the
+        line scan.
+
+        Parameters
+        ----------
+        heights : float or np.ndarray
+            Heights for which to compute the bearing area.
+
+        Returns
+        -------
+        lower_bound : float or np.ndarray
+            Lower bound on fractional area above a threshold height.
+        upper_bound : float or np.ndarray
+            Upper bound on fractional area above a threshold height.
+        """
+        el_min = np.searchsorted(self._el_min_heights, heights)
+        el_max = np.searchsorted(self._el_max_heights, heights)
+        return (self._nb_els - el_min) / self._nb_els, (self._nb_els - el_max) / self._nb_els
+
+
+class Uniform1DBearingArea(UniformBearingArea):
     """
-    Accelerated bearing area calculation for nonuniform line scans.
+    Accelerated bearing area calculation for uniform line scans.
     """
 
     def __init__(self, dx, h, is_periodic):
         self._dx = dx
-        self._h = np.asanyarray(h, dtype=float)
+        self._h = np.ascontiguousarray(h, dtype=float)
         self._is_periodic = is_periodic
 
-        if is_periodic:
-            # Element indices, sorted by min height of element
-            self._el_sort_by_min = np.argsort(np.minimum(np.roll(self._h, -1), np.roll(self._h, 1)))
-            # Element indices, sorted by max height of element
-            self._el_sort_by_max = np.argsort(np.maximum(np.roll(self._h, -1), np.roll(self._h, 1)))
+        if self._is_periodic:
+            self._nb_els = self._h.size
+            self._el_min_heights = np.sort(np.minimum(self._h, np.roll(self._h, -1)))
+            self._el_max_heights = np.sort(np.maximum(self._h, np.roll(self._h, -1)))
         else:
-            # Element indices, sorted by min height of element
-            self._el_sort_by_min = np.argsort(np.minimum(self._h[:-1], self._h[1:]))
-            # Element indices, sorted by max height of element
-            self._el_sort_by_max = np.argsort(np.maximum(self._h[:-1], self._h[1:]))
+            self._nb_els = self._h.size - 1
+            self._el_min_heights = np.sort(np.minimum(self._h[:-1], self._h[1:]))
+            self._el_max_heights = np.sort(np.maximum(self._h[:-1], self._h[1:]))
 
     def __call__(self, heights):
         """
@@ -83,11 +105,51 @@ class Uniform1DBearingArea:
             return _SurfaceTopographyPP.uniform1d_bearing_area(self._dx, self._h, self._is_periodic,
                                                                heights.astype(float))
 
-    def bounds(self, heights):
+
+class Uniform2DBearingArea(UniformBearingArea):
+    """
+    Accelerated bearing area calculation for topographies.
+    """
+
+    def __init__(self, dx, dy, h, is_periodic):
+        self._dx = dx
+        self._dy = dy
+        self._h = np.ascontiguousarray(h, dtype=float)
+        self._is_periodic = is_periodic
+
+        nx, ny = self._h.shape
+        if self._is_periodic:
+            self._nb_els = 2 * nx * ny
+            self._el_min_heights = np.sort(np.ravel([
+                np.minimum.reduce([self._h, np.roll(self._h, (-1, 0)), np.roll(self._h, (0, -1))]),
+                np.minimum.reduce([np.roll(self._h, (-1, -1)), np.roll(self._h, (-1, 0)), np.roll(self._h, (0, -1))])
+            ]))
+            self._el_max_heights = np.sort(np.ravel([
+                np.maximum.reduce([self._h, np.roll(self._h, (-1, 0)), np.roll(self._h, (0, -1))]),
+                np.maximum.reduce([np.roll(self._h, (-1, -1)), np.roll(self._h, (-1, 0)), np.roll(self._h, (0, -1))])
+            ]))
+        else:
+            self._nb_els = 2 * (nx - 1) * (ny - 1)
+            self._el_min_heights = np.sort(np.ravel([
+                np.minimum.reduce([self._h[:-1, :-1], self._h[1:, :-1], self._h[:-1, 1:]]),
+                np.minimum.reduce([self._h[1:, 1:], self._h[1:, :-1], self._h[:-1, 1:]])
+            ]))
+            self._el_max_heights = np.sort(np.ravel([
+                np.maximum.reduce([self._h[:-1, :-1], self._h[1:, :-1], self._h[:-1, 1:]]),
+                np.maximum.reduce([self._h[1:, 1:], self._h[1:, :-1], self._h[:-1, 1:]])
+            ]))
+
+    def __call__(self, heights):
         """
-        Compute bounds on the bearing area for a specific height. These bounds
-        can be computed O(log(N)) time, where N is the number of elements in the
-        line scan.
+        Compute the bearing area for a specific height.
+
+        The bearing area as a function of height is also known as the
+        Abbott-Firestone curve. If expressed as a fractional area, it is the
+        cumulative distribution function of the surface heights. This function
+        returns this fractional area.
+
+        The function has complexity O(N) where N is the number of elements in
+
 
         Parameters
         ----------
@@ -96,24 +158,15 @@ class Uniform1DBearingArea:
 
         Returns
         -------
-        lower_bound : float or np.ndarray
-            Lower bound on fractional area above a threshold height.
-        upper_bound : float or np.ndarray
-            Upper bound on fractional area above a threshold height.
+        fractional_area : float or np.ndarray
+            Fractional area above a the threshold height.
         """
-        if self._is_periodic:
-            el_min_heights = np.minimum(np.roll(self._h, -1), np.roll(self._h, 1))
-            el_max_heights = np.maximum(np.roll(self._h, -1), np.roll(self._h, 1))
+        if np.isscalar(heights):
+            return _SurfaceTopographyPP.uniform2d_bearing_area(self._dx, self._dy, self._h, self._is_periodic,
+                                                               np.array([heights], dtype=float))[0]
         else:
-            el_min_heights = np.minimum(self._h[:-1], self._h[1:])
-            el_max_heights = np.maximum(self._h[:-1], self._h[1:])
-        el_min = np.searchsorted(el_min_heights[self._el_sort_by_min], heights)
-        el_max = np.searchsorted(el_max_heights[self._el_sort_by_max], heights)
-        if self._is_periodic:
-            nb_els = len(self._h)
-        else:
-            nb_els = len(self._h) - 1
-        return (nb_els - el_min) / nb_els, (nb_els - el_max) / nb_els
+            return _SurfaceTopographyPP.uniform2d_bearing_area(self._dx, self._dy, self._h, self._is_periodic,
+                                                               heights.astype(float))
 
 
 def bearing_area(self, heights=None):
@@ -140,18 +193,9 @@ def bearing_area(self, heights=None):
         Instance of a class that caches the bearing area calculation.
     """
     if self.dim == 1:
-        b = Uniform1DBearingArea(self.pixel_size[0], self.heights(), self.is_periodic)
+        b = Uniform1DBearingArea(*self.pixel_size, self.heights(), self.is_periodic)
     elif self.dim == 2:
-        h = self.heights()
-        dx, dy = self.pixel_size
-        if np.isscalar(heights):
-            return _SurfaceTopographyPP.uniform2d_bearing_area(dx, dy, np.ascontiguousarray(h, dtype=float),
-                                                               self.is_periodic,
-                                                               np.array([heights], dtype=float))[0]
-        else:
-            return _SurfaceTopographyPP.uniform2d_bearing_area(dx, dy, np.ascontiguousarray(h, dtype=float),
-                                                               self.is_periodic,
-                                                               heights.astype(float))
+        b = Uniform2DBearingArea(*self.pixel_size, self.heights(), self.is_periodic)
     else:
         raise NotImplementedError('Bearing area is only implemented for 1D line scans and 2D topography maps.')
 
--- test/test_bearing_area.py
+++ test/test_bearing_area.py
@@ -89,7 +89,7 @@ def test_bearing_area_uniform_is_monotonous(plot=False):
     assert (np.diff(P) < 0).all()
 
 
-def test_bearing_area_bounds():
+def test_bearing_area_bounds_1d():
     t = fourier_synthesis((64,), (1,), 0.8, rms_slope=0.1, periodic=False)
     mn = t.min()
     mx = t.max()
@@ -117,6 +117,37 @@ def test_bearing_area_bounds():
 
     # Test uniform periodic
     t = fourier_synthesis((64,), (1,), 0.8, rms_slope=0.1, periodic=True)
+    mn = t.min()
+    mx = t.max()
+    heights = np.linspace(mn, mx, 100)
+
+    # Test bounds
+    ba = t.bearing_area()
+    lower, upper = ba.bounds(heights)
+    eps = 1e-12
+    np.testing.assert_array_less(lower, upper + eps)
+    np.testing.assert_array_less(lower, ba(heights) + eps)
+    np.testing.assert_array_less(ba(heights), upper + eps)
+
+
+def test_bearing_area_bounds_2d():
+    t = fourier_synthesis((64, 63), (1, 1), 0.8, rms_slope=0.1, periodic=False)
+    mn = t.min()
+    mx = t.max()
+    heights = np.linspace(mn, mx, 100)
+
+    # Test uniform
+    ba = t.bearing_area()
+
+    # Test bounds
+    lower, upper = ba.bounds(heights)
+    eps = 1e-12
+    np.testing.assert_array_less(lower, upper + eps)
+    np.testing.assert_array_less(lower, ba(heights) + eps)
+    np.testing.assert_array_less(ba(heights), upper + eps)
+
+    # Test uniform periodic
+    t = fourier_synthesis((64, 63), (1, 1), 0.8, rms_slope=0.1, periodic=True)
     mn = t.min()
     mx = t.max()
     heights = np.linspace(mn, mx, 100)
