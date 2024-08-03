BUG: Fixed computation of derivatives for topographies with missing data

SurfaceTopography/Uniform/Derivative.py
@@ -198,14 +198,15 @@ def derivative(
         A function that takes as argument the output of FFT.fftfreq and
         returns a mask that will be multiplied with the Fourier transformed
         topography. This can be used to implement Fourier filtering before
-        computing the derivative. (Default: None)
+        computing the derivative. This only works if interpolation is set to
+        'fourier'. (Default: None)
     interpolation : str, optional
         Interpolation method to use for fractional scale factors. Use
         'linear' for a local liner interpolation or 'fourier' for global
         Fourier interpolation. Set to 'disable' to raise an error when
         interpolation is necessary. Note that Fourier interpolation carries
-        large errors for nonperiodic topographies and should be used with
-        care. (Default: 'linear')
+        large errors for nonperiodic topographies and should not be used with
+        windowing. (Default: 'linear')
     progress_callback : func, optional
         Function taking iteration and the total number of iterations as
         arguments for progress reporting. (Default: None)
@@ -232,17 +233,49 @@ def derivative(
     if operator is None:
         operator = _get_default_derivative_operator(n, self.dim)
 
+    nb_grid_pts = np.array(self.nb_grid_pts)
+    pixel_size = np.array(self.pixel_size)
+
     if interpolation == "linear":
+        heights = None
         linear = self.interpolate_linear()
         positions = np.transpose(self.positions())
-    elif interpolation == "fourier" or interpolation == "disable":
+        integer_positions = None
+    elif interpolation == "fourier":
+        heights = None
         linear = None
+        positions = None
+        integer_positions = None
+
+        # Return FFT object (this will only be initialized once and reused in subsequent calls)
+        fft = self.make_fft()
+
+        # These fields are reused when this function is called multiple times
+        real_field = fft.real_space_field("real_temporary", 1)
+        fourier_field = fft.fourier_space_field("complex_temporary", 1)
+        np.array(real_field, copy=False)[...] = self.heights()
+        fft.fft(real_field, fourier_field)
+
+        # Apply mask function
+        if mask_function is not None:
+            np.array(fourier_field, copy=False)[...] *= mask_function(
+                (fft.fftfreq.T / pixel_size).T
+            )
+
+        fourier_array = np.array(fourier_field, copy=False)
+        fourier_copy = fourier_array.copy()
+    elif interpolation == "disable":
+        heights = self.heights()
+        linear = None
+        positions = None
+        if self.dim == 1:
+            integer_positions = np.arange(nb_grid_pts[0])
+        else:
+            integer_positions = np.mgrid[: nb_grid_pts[0], : nb_grid_pts[1]]
     else:
         raise ValueError(
             "`interpolation` argument must be either 'linear', 'fourier' or 'disable'."
         )
-
-    pixel_size = np.array(self.pixel_size)
 
     nb_scale_factors = 1
     scale_factor_is_array = False
@@ -284,6 +317,12 @@ def derivative(
     derivatives = []
     operators = toiter(operator)
     for i, op in enumerate(operators):
+        if isinstance(op, muFFT.FourierDerivative) and not interpolation == "fourier":
+            raise ValueError(
+                "Operator is a muFFT.FourierDerivative but interpolation is not set "
+                "to 'fourier'."
+            )
+
         der = []
         for j, s in enumerate(scale_factor):
             if progress_callback is not None:
@@ -292,69 +331,72 @@ def derivative(
                 )
             s = np.array(s) * np.ones_like(pixel_size)
             scaled_pixel_size = s * pixel_size
-            interpolation_required = np.any(s - s.astype(int) != 0)
-            if interpolation_required:
-                if interpolation == "disabled":
-                    raise ValueError(
-                        "Interpolation is required to compute derivative at the desired scale but is "
-                        "explicitly disabled through the `interpolation` argument."
+            interpolation_required = (
+                np.any(s - s.astype(int) != 0) or interpolation == "fourier"
+            )
+            if interpolation_required and interpolation == "disabled":
+                raise ValueError(
+                    "Interpolation is required to compute derivative at the "
+                    "desired scale but is explicitly disabled through the "
+                    "`interpolation` argument."
+                )
+
+            if interpolation == "fourier":
+                # We use Fourier interpolation
+                if self.has_undefined_data:
+                    raise UndefinedDataError(
+                        "This topography has undefined data (missing data points). "
+                        "Derivatives cannot be computed via Fourier interpolation "
+                        "for topographies with missing data points."
                     )
-                elif linear is not None:
-                    # We need to interpolate using the linear interpolator
-                    lbounds = np.array(op.lbounds)
-                    stencil = np.array(op.stencil)
-                    _der = np.zeros_like(real_field)
-                    for stencil_coordinate, stencil_value in np.ndenumerate(stencil):
-                        if stencil_value:
-                            stencil_positions = (
-                                positions
-                                + (lbounds + stencil_coordinate) * scaled_pixel_size
+
+                # We can use the Fourier trick to compute the derivative; this gives the exact stencil derivative if
+                # interpolation is not required and the Fourier-interpolated derivative for fractional scale factors
+                fourier_array[...] = fourier_copy * op.fourier((fft.fftfreq.T * s).T)
+                fft.ifft(fourier_field, real_field)
+                _der = np.array(real_field, copy=False) * fft.normalisation
+            elif interpolation == "linear":
+                # We use linear interpolator
+                lbounds = np.array(op.lbounds)
+                stencil = np.array(op.stencil)
+                _der = np.zeros(nb_grid_pts)
+                for stencil_coordinate, stencil_value in np.ndenumerate(stencil):
+                    if stencil_value:
+                        stencil_positions = (
+                            positions
+                            + (lbounds + stencil_coordinate) * scaled_pixel_size
+                        )
+                        # We enforce periodicity here but will trim the (erroneous)
+                        # boundary region below
+                        if self.dim == 1:
+                            _der += stencil_value * linear(
+                                stencil_positions, periodic=True
                             )
-                            # We enforce periodicity here but will trim the (erroneous) boundary region below
-                            if self.dim == 1:
-                                _der += stencil_value * linear(
-                                    stencil_positions, periodic=True
-                                )
-                            else:
-                                _der += stencil_value * linear(
-                                    *stencil_positions.T, periodic=True
-                                )
-                else:
-                    # We use Fourier interpolation
-                    if self.has_undefined_data:
-                        raise UndefinedDataError(
-                            "This topography has undefined data (missing data points). Derivatives cannot be "
-                            "computed via Fourier interpolation for topographies with missing data points."
-                        )
-
-                    # Return FFT object (this will only be initialized once and reused in subsequent calls)
-                    fft = self.make_fft()
-
-                    # These fields are reused when this function is called multiple times
-                    real_field = fft.real_space_field("real_temporary", 1)
-                    fourier_field = fft.fourier_space_field("complex_temporary", 1)
-                    np.array(real_field, copy=False)[...] = self.heights()
-                    fft.fft(real_field, fourier_field)
-
-                    # Apply mask function
-                    if mask_function is not None:
-                        np.array(fourier_field, copy=False)[...] *= mask_function(
-                            (fft.fftfreq.T / pixel_size).T
-                        )
-
-                    fourier_array = np.array(fourier_field, copy=False)
-                    fourier_copy = fourier_array.copy()
-
-                    # We can use the Fourier trick to compute the derivative; this gives the exact stencil derivative if
-                    # interpolation is not required and the Fourier-interpolated derivative for fractional scale factors
-                    fourier_array[...] = fourier_copy * op.fourier(
-                        (fft.fftfreq.T * s).T
-                    )
-                    fft.ifft(fourier_field, real_field)
-                    _der = np.array(real_field, copy=False) * fft.normalisation
+                        else:
+                            _der += stencil_value * linear(
+                                *stencil_positions.T, periodic=True
+                            )
             else:
-                # No interpolation
-                _der = op.apply(self.heights())
+                # Interpolation disabled
+                lbounds = np.array(op.lbounds)
+                stencil = np.array(op.stencil)
+                _der = np.zeros(nb_grid_pts)
+                s = s.astype(int)
+                for stencil_coordinate, stencil_value in np.ndenumerate(stencil):
+                    if stencil_value:
+                        stencil_positions = (
+                            (integer_positions.T + (lbounds + stencil_coordinate) * s)
+                            % nb_grid_pts
+                        ).T
+                        # We enforce periodicity here but will trim the (erroneous)
+                        # boundary region below
+                        if self.dim == 1:
+                            _der += stencil_value * heights[stencil_positions]
+                        else:
+                            _der += (
+                                stencil_value
+                                * heights[stencil_positions[0], stencil_positions[1]]
+                            )
 
             if not is_periodic:
                 _der = trim_nonperiodic(_der, s, op)
@@ -367,7 +409,9 @@ def derivative(
                 _der = np.ma.masked_invalid(_der)
                 if _der.mask.sum() == _der.size:
                     raise UndefinedDataError(
-                        "All data points in the derivative are undefined (probably because of missing data points in the topography). Derivatives cannot be computed."
+                        "All data points in the derivative are undefined (probably "
+                        "because of missing data points in the topography). "
+                        "Derivatives cannot be computed."
                     )
 
             if scale_factor_is_array:
@@ -449,6 +493,7 @@ def fourier_derivative(self, scale_factor=None, distance=None, mask_function=Non
     return self.derivative(
         1,
         operator=(muFFT.FourierDerivative(dim, i) for i in range(dim)),
+        interpolation="fourier",
         scale_factor=scale_factor,
         distance=distance,
         mask_function=mask_function,

SurfaceTopography/Uniform/ScalarParameters.py
@@ -55,12 +55,17 @@ def Rq(topography):
         Root mean square height value.
     """
     if topography.is_domain_decomposed:
-        raise NotImplementedError('`Rq` does not support MPI-decomposed topographies.')
+        raise NotImplementedError("`Rq` does not support MPI-decomposed topographies.")
 
     n = np.prod(topography.nb_grid_pts)
     reduction = Reduction(topography._communicator)
     profile = topography.heights()
-    return np.sqrt(reduction.sum((profile - reduction.sum(profile, axis=0) / topography.nb_grid_pts[0]) ** 2) / n)
+    return np.sqrt(
+        reduction.sum(
+            (profile - reduction.sum(profile, axis=0) / topography.nb_grid_pts[0]) ** 2
+        )
+        / n
+    )
 
 
 def Sq(topography):
@@ -80,14 +85,16 @@ def Sq(topography):
         Root mean square height value.
     """
     if topography.dim <= 1:
-        raise ValueError('Areal rms height can only be computed for topographies, not line scans.')
+        raise ValueError(
+            "Areal rms height can only be computed for topographies, not line scans."
+        )
     elif topography.dim == 2:
         n = np.prod(topography.nb_grid_pts)
         reduction = Reduction(topography._communicator)
         profile = topography.heights()
         return np.sqrt(reduction.sum((profile - reduction.sum(profile) / n) ** 2) / n)
     else:
-        raise ValueError(f'Cannot handle topographies of dimension {topography.dim}')
+        raise ValueError(f"Cannot handle topographies of dimension {topography.dim}")
 
 
 def rms_gradient(topography, short_wavelength_cutoff=None, window=None, direction=None):
@@ -121,15 +128,25 @@ def rms_gradient(topography, short_wavelength_cutoff=None, window=None, directio
     if short_wavelength_cutoff is not None:
         topography = topography.window(window=window, direction=direction)
     if topography.dim <= 1:
-        raise ValueError('RMS gradient can only be computed for topographies, not line scans.')
+        raise ValueError(
+            "RMS gradient can only be computed for topographies, not line scans."
+        )
     elif topography.dim == 2:
-        mask_function = None if short_wavelength_cutoff is None else \
-            lambda frequency: (frequency[0] ** 2 + frequency[1] ** 2) < 1 / short_wavelength_cutoff ** 2
-        slx, sly = topography.derivative(1, mask_function=mask_function)
+        mask_function = (
+            None
+            if short_wavelength_cutoff is None
+            else lambda frequency: (frequency[0] ** 2 + frequency[1] ** 2)
+            < 1 / short_wavelength_cutoff**2
+        )
+        slx, sly = topography.derivative(
+            1,
+            mask_function=mask_function,
+            interpolation="linear" if mask_function is None else "fourier",
+        )
         reduction = Reduction(topography._communicator)
-        return np.sqrt(reduction.mean(slx ** 2 + sly ** 2))
+        return np.sqrt(reduction.mean(slx**2 + sly**2))
     else:
-        raise ValueError(f'Cannot handle topographies of dimension {topography.dim}')
+        raise ValueError(f"Cannot handle topographies of dimension {topography.dim}")
 
 
 def Rdq(topography, short_wavelength_cutoff=None, window=None, direction=None):
@@ -163,16 +180,27 @@ def Rdq(topography, short_wavelength_cutoff=None, window=None, direction=None):
     """
     if short_wavelength_cutoff is not None:
         topography = topography.window(window=window, direction=direction)
-    mask_function = None if short_wavelength_cutoff is None else \
-        lambda frequency: frequency[0] ** 2 < 1 / short_wavelength_cutoff ** 2
+    mask_function = (
+        None
+        if short_wavelength_cutoff is None
+        else lambda frequency: frequency[0] ** 2 < 1 / short_wavelength_cutoff**2
+    )
     if topography.dim == 1:
-        dx = topography.derivative(1, mask_function=mask_function)
+        dx = topography.derivative(
+            1,
+            mask_function=mask_function,
+            interpolation="linear" if mask_function is None else "fourier",
+        )
     elif topography.dim == 2:
-        dx, dy = topography.derivative(1, mask_function=mask_function)
+        dx, dy = topography.derivative(
+            1,
+            mask_function=mask_function,
+            interpolation="linear" if mask_function is None else "fourier",
+        )
     else:
-        raise ValueError(f'Cannot handle topographies of dimension {topography.dim}')
+        raise ValueError(f"Cannot handle topographies of dimension {topography.dim}")
     reduction = Reduction(topography._communicator)
-    return np.sqrt(reduction.mean(dx ** 2))
+    return np.sqrt(reduction.mean(dx**2))
 
 
 def Rddq(topography, short_wavelength_cutoff=None, window=None, direction=None):
@@ -206,19 +234,32 @@ def Rddq(topography, short_wavelength_cutoff=None, window=None, direction=None):
     """
     if short_wavelength_cutoff is not None:
         topography = topography.window(window=window, direction=direction)
-    mask_function = None if short_wavelength_cutoff is None else \
-        lambda frequency: frequency[0] ** 2 < 1 / short_wavelength_cutoff ** 2
+    mask_function = (
+        None
+        if short_wavelength_cutoff is None
+        else lambda frequency: frequency[0] ** 2 < 1 / short_wavelength_cutoff**2
+    )
     if topography.dim == 1:
-        d2x = topography.derivative(2, mask_function=mask_function)
+        d2x = topography.derivative(
+            2,
+            mask_function=mask_function,
+            interpolation="linear" if mask_function is None else "fourier",
+        )
     elif topography.dim == 2:
-        d2x, d2y = topography.derivative(2, mask_function=mask_function)
+        d2x, d2y = topography.derivative(
+            2,
+            mask_function=mask_function,
+            interpolation="linear" if mask_function is None else "fourier",
+        )
     else:
-        raise ValueError(f'Cannot handle topographies of dimension {topography.dim}')
+        raise ValueError(f"Cannot handle topographies of dimension {topography.dim}")
     reduction = Reduction(topography._communicator)
-    return np.sqrt(reduction.mean(d2x ** 2))
+    return np.sqrt(reduction.mean(d2x**2))
 
 
-def rms_laplacian(topography, short_wavelength_cutoff=None, window=None, direction=None):
+def rms_laplacian(
+    topography, short_wavelength_cutoff=None, window=None, direction=None
+):
     """
     Compute the root mean square amplitude of the Laplacian (i.e. the sum of
     second derivatives in x- and y-directions) of a topography on a uniform
@@ -249,18 +290,30 @@ def rms_laplacian(topography, short_wavelength_cutoff=None, window=None, directi
     if short_wavelength_cutoff is not None:
         topography = topography.window(window=window, direction=direction)
     if topography.dim == 1:
-        raise ValueError('RMS Laplacian can only be computed for topographies, not line scans.')
+        raise ValueError(
+            "RMS Laplacian can only be computed for topographies, not line scans."
+        )
     elif topography.dim == 2:
-        mask_function = None if short_wavelength_cutoff is None else \
-            lambda frequency: (frequency[0] ** 2 + frequency[1] ** 2) < 1 / short_wavelength_cutoff ** 2
-        curv = topography.derivative(2, mask_function=mask_function)
+        mask_function = (
+            None
+            if short_wavelength_cutoff is None
+            else lambda frequency: (frequency[0] ** 2 + frequency[1] ** 2)
+            < 1 / short_wavelength_cutoff**2
+        )
+        curv = topography.derivative(
+            2,
+            mask_function=mask_function,
+            interpolation="linear" if mask_function is None else "fourier",
+        )
         reduction = Reduction(topography._communicator)
         return np.sqrt(reduction.mean((curv[0] + curv[1]) ** 2))
     else:
-        raise ValueError(f'Cannot handle topographies of dimension {topography.dim}')
+        raise ValueError(f"Cannot handle topographies of dimension {topography.dim}")
 
 
-def rms_curvature_from_area(topography, short_wavelength_cutoff=None, window=None, direction=None):
+def rms_curvature_from_area(
+    topography, short_wavelength_cutoff=None, window=None, direction=None
+):
     """
     Compute the root mean square amplitude of the curvature of a
     topography stored on a uniform grid. The topography must be
@@ -289,19 +342,36 @@ def rms_curvature_from_area(topography, short_wavelength_cutoff=None, window=Non
     rms_curvature : float
         Root mean square curvature value.
     """
-    return rms_laplacian(topography, short_wavelength_cutoff=short_wavelength_cutoff, window=window,
-                         direction=direction) / 2
+    return (
+        rms_laplacian(
+            topography,
+            short_wavelength_cutoff=short_wavelength_cutoff,
+            window=window,
+            direction=direction,
+        )
+        / 2
+    )
 
 
 # Register analysis functions from this module
-UniformTopographyInterface.register_function('rms_height_from_profile', Rq, deprecated=True)
-UniformTopographyInterface.register_function('Rq', Rq)
-UniformTopographyInterface.register_function('rms_height_from_area', Sq, deprecated=True)
-UniformTopographyInterface.register_function('Sq', Sq)
-UniformTopographyInterface.register_function('rms_gradient', rms_gradient)
-UniformTopographyInterface.register_function('rms_slope_from_profile', Rdq, deprecated=True)
-UniformTopographyInterface.register_function('Rdq', Rdq)
-UniformTopographyInterface.register_function('rms_curvature_from_profile', Rddq, deprecated=True)
-UniformTopographyInterface.register_function('Rddq', Rddq)
-UniformTopographyInterface.register_function('rms_laplacian', rms_laplacian)
-UniformTopographyInterface.register_function('rms_curvature_from_area', rms_curvature_from_area)
+UniformTopographyInterface.register_function(
+    "rms_height_from_profile", Rq, deprecated=True
+)
+UniformTopographyInterface.register_function("Rq", Rq)
+UniformTopographyInterface.register_function(
+    "rms_height_from_area", Sq, deprecated=True
+)
+UniformTopographyInterface.register_function("Sq", Sq)
+UniformTopographyInterface.register_function("rms_gradient", rms_gradient)
+UniformTopographyInterface.register_function(
+    "rms_slope_from_profile", Rdq, deprecated=True
+)
+UniformTopographyInterface.register_function("Rdq", Rdq)
+UniformTopographyInterface.register_function(
+    "rms_curvature_from_profile", Rddq, deprecated=True
+)
+UniformTopographyInterface.register_function("Rddq", Rddq)
+UniformTopographyInterface.register_function("rms_laplacian", rms_laplacian)
+UniformTopographyInterface.register_function(
+    "rms_curvature_from_area", rms_curvature_from_area
+)

test/test_derivatives.py
@@ -169,7 +169,8 @@ def test_third_derivatives_fourier_vs_finite_differences(plot=False):
         fig.show()
 
 
-def test_scale_factor():
+@pytest.mark.parametrize("interpolation", ["disable", "linear", "fourier"])
+def test_scale_factor(interpolation):
     nx = 8
     sx = 1
 
@@ -177,10 +178,14 @@ def test_scale_factor():
     topography1 = UniformLineScan(topography.heights()[::2], sx, periodic=True)
     topography2 = UniformLineScan(topography.heights()[1::2], sx, periodic=True)
 
-    d1 = topography.derivative(1, scale_factor=1)
-    d2 = topography.derivative(1, scale_factor=np.uint32(2))
-    d3 = topography1.derivative(1, scale_factor=1)
-    d4 = topography2.derivative(1, scale_factor=1)
+    d0 = topography.derivative(1, scale_factor=1, interpolation="disable")
+    d1 = topography.derivative(1, scale_factor=1, interpolation=interpolation)
+
+    np.testing.assert_allclose(d0, d1)
+
+    d2 = topography.derivative(1, scale_factor=np.uint32(2), interpolation=interpolation)
+    d3 = topography1.derivative(1, scale_factor=1, interpolation=interpolation)
+    d4 = topography2.derivative(1, scale_factor=1, interpolation=interpolation)
 
     assert len(d2) == len(d1)
     np.testing.assert_allclose(d3, d2[::2])
@@ -190,9 +195,9 @@ def test_scale_factor():
     topography1 = UniformLineScan(topography.heights()[::2], sx, periodic=False)
     topography2 = UniformLineScan(topography.heights()[1::2], sx, periodic=False)
 
-    d1, d2 = topography.derivative(1, scale_factor=[1, 2])
-    d3 = topography1.derivative(1, scale_factor=1)
-    d4 = topography2.derivative(1, scale_factor=1)
+    d1, d2 = topography.derivative(1, scale_factor=[1, 2], interpolation=interpolation)
+    d3 = topography1.derivative(1, scale_factor=1, interpolation=interpolation)
+    d4 = topography2.derivative(1, scale_factor=1, interpolation=interpolation)
 
     assert len(d2) == len(d1) - 1
     np.testing.assert_allclose(d3, d2[::2])
@@ -204,7 +209,7 @@ def test_scale_factor():
         (nx, ny), (sx, sy), 0.8, rms_height=1.0, periodic=False
     )
 
-    dx, dy = topography.derivative(1, scale_factor=[1, 2, 4])
+    dx, dy = topography.derivative(1, scale_factor=[1, 2, 4], interpolation=interpolation)
     dx1, dx2, dx4 = dx
     dy1, dy2, dy4 = dy
 
@@ -221,7 +226,7 @@ def test_scale_factor():
     assert dy4.shape[0] == nx - 4
     assert dy4.shape[1] == ny - 4
 
-    dx, dy = topography.derivative(1, scale_factor=[(1, 4), (2, 1), (4, 2)])
+    dx, dy = topography.derivative(1, scale_factor=[(1, 4), (2, 1), (4, 2)], interpolation=interpolation)
     dxn1, dxn2, dxn4 = dx
     dyn4, dyn1, dyn2 = dy
     np.testing.assert_allclose(dxn1, dx1[:, :-3])

test/test_missing_data.py
@@ -128,9 +128,15 @@ def test_exception(make_topography_with_missing_data, dim, is_nonuniform, height
     assert np.isfinite(topo.rms_height_from_area())
     assert np.isfinite(topo.rms_slope_from_profile())
     assert np.isfinite(topo.rms_gradient())
-    assert np.isfinite(topo.rms_curvature_from_profile())
-    assert np.isfinite(topo.rms_curvature_from_area())
-    assert np.isfinite(topo.rms_laplacian())
+
+    # This raises an exception because the there are not enough data points for the
+    # computation of a second derivative
+    with pytest.raises(UndefinedDataError):
+        topo.rms_curvature_from_profile()
+    with pytest.raises(UndefinedDataError):
+        topo.rms_curvature_from_area()
+    with pytest.raises(UndefinedDataError):
+        topo.rms_laplacian()
 
     # This raises an exception because the autocorrelation is computed using an FFT
     # which cannot be computed with missing data points