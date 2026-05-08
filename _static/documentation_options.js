288151fe654aeabdbfe73e7ee43951a4c9da35fb
GitHub Action <action@github.com>
2026-05-08 12:42:43 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.21.1.dev22+gd18760ec',
+    VERSION: '1.21.1.dev24+g5eeb4e3e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
